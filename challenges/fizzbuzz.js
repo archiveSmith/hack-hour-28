431d2dfdce5f1b9487791836eeeb15d54cccbf68
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-03-27 09:33:08 -0700

    complete fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,44 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+    let myArr = [];
+
+    for(let i = 1; i <= num; i++){
+        let track = i;
+        if(i % 3 === 0 && i % 5 === 0){
+          track = "fizzbuzz";
+          myArr.push(track)
+        }
+       if(i % 3 === 0){
+          track = "fizz";
+          myArr.push(track);
+       } else if(i % 5 === 0){
+           track = "buzz";
+           myArr.push(track);
+       } else {
+           track = i;
+        myArr.push(track);
+       }
+    }
+    return myArr;
 
 }
+console.log(fizzbuzz(16));  //-> [ 1,
+//                     2,
+//                     'fizz',
+
+//                     4,
+//                     'buzz',
+//                     'fizz',
+//                     7,
+//                     8,
+//                     'fizz',
+//                     'buzz',
+//                     11,
+//                     'fizz',
+//                     13,
+//                     14,
+//                     'fizzbuzz',
+//                     16 ]
 
 module.exports = fizzbuzz;
