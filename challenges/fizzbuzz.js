b829cfa4768a7a6aab60ce646810cbb32d51f4be
Aidan Noble-Goodman <aidan_noble-goodman@Aidans-MacBook-Air.local>
2019-03-27 09:16:08 -0700

    FizzBuzz Challenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,14 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  if(num % 3 === 0 && num % 5 === 0){
+    return 'fizzbuzz';
+  } else if(num % 3 === 0){
+    return 'buzz'
+  } else if(num % 5 === 0){
+    return 'fizz'
+  }
+  return num
 }
 
 module.exports = fizzbuzz;
