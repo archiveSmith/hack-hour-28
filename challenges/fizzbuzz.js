71cee80366e950ec3259327c3c03a4b4334161c7
Alex Park <apark0720@gmail.com>
2019-03-27 09:43:55 -0700

    complete fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,5 +1,7 @@
 // Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
-// fizzbuzz(16);  -> [ 1,
+fizzbuzz(16);  
+console.log("clg: fizzbuzz(16)", fizzbuzz(16))
+// -> [ 1,
 //                     2,
 //                     'fizz',
 //                     4,
@@ -17,7 +19,21 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  let fbArr = [];
 
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      fbArr.push('fizzbuzz')
+    } else if (i % 3 === 0) {
+      fbArr.push('fizz')
+    } else if (i % 5 === 0) {
+      fbArr.push('buzz')
+    } else {
+      fbArr.push(i)
+    }
+  }
+  
+  return fbArr;
 }
 
 module.exports = fizzbuzz;
