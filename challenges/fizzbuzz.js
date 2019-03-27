433e72b90c493d5ab5e95e45e330727d7b7cab98
chetbaby <cbaby@CBMB.local>
2019-03-27 09:24:11 -0700

    finished

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,22 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  // create empty Aject that has 'fizz' and 'buzz' and 'fizzbuzz'
+  let returnArr = [];
+  let fizzBuzzTable = {};
+  // for loop push i into empty Array
+  for (let i = 1; i <= num; i += 1) {
+    // have conditional for each condition as replacement for i
+    if (i % 3 === 0 && i % 5 === 0) {
+      returnArr.push("fizzbuzz");
+    } else if (i % 3 === 0) {
+      returnArr.push("fizz");
+    } else if (i % 5 === 0) {
+      returnArr.push("buzz");
+    } else returnArr.push(i);
+  }
+  //   return array
+  return returnArr;
 }
-
+fizzbuzz(16); //?
 module.exports = fizzbuzz;
