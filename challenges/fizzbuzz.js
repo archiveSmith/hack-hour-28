ac79c3440c43cf6dc3b43a91e75e5c056548bd48
Sam Selfridge <Sam.selfridge@gmail.com>
2019-03-27 09:26:10 -0700

    Fizzing and Buzzing

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,5 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers
+//  divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -17,7 +18,24 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    const array = [];
+    for(let i = 1; i <= num ; i++){
+        let output;
+        if(i % 3 === 0 || i % 5 === 0){
+            output = '';
+            if(i % 3 === 0) output = 'fizz'
+            if(i % 5 === 0) output = output + 'buzz'
+        }else{
+            console.log(i);
+            output = i;
+        }
+        array.push(output);
+    }
+    return array;
 }
 
+
+let out = fizzbuzz(20);
+
+console.log('fizzbuzz(20):', out);
 module.exports = fizzbuzz;
