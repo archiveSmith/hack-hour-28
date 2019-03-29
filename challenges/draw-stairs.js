fa44014b3c54e6dffa9031f4745123ffcb60d00d
Sam Ryoo <samryoo@gmail.com>
2019-03-29 11:28:44 -0700

    finished

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,11 @@
 */
 
 function drawStairs(n) {
-
+    let result = '';
+    for (let i =1; i<=n; i++) {
+      result += ' '.repeat(n-i) + '*'.repeat(i) + '\n';
+    }
+    return result;
 }
-
 
 module.exports = drawStairs;
