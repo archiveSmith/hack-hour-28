a5d228022f07df457f39be86157e764aaed9433d
Sam Selfridge <Sam.selfridge@gmail.com>
2019-03-29 09:50:33 -0700

    drawing stairs with a helper method

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,9 +13,35 @@
 
 */
 
-function drawStairs(n) {
-
+function makeLevel(total,currLen){
+    let output = '';
+    for(let i = 1 ; i <= total;i++){
+        let newChar;
+        if(i > currLen){
+            newChar = ' '
+        } else {
+            newChar = '*'
+        }
+        output = newChar + output;
+    }
+    return output + '\n';
 }
 
+
+function drawStairs(n) {
+    let output =''
+    for(let level = 1;level <= n;level++){
+        let newLevel = makeLevel(n,level);
+        output += newLevel
+    }
+    return output;
+}
+
+let output = drawStairs(8);
+output
+// console.log(`output:'${output}'`);
+// console.log('output.length: ', output.length);
+output
+console.log(output);
 
 module.exports = drawStairs;
