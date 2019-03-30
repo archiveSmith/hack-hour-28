73218fb4af77f012fd672be3c9b8a2dcb13b933c
Karen Pinilla <pinillakaren11@gmail.com>
2019-03-30 13:33:43 -0700

    finished hack hour

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,43 @@
 
 
 function modemean(array) {
-
+    //variables for mode
+    let modeObj = {}
+    let maxFreq = 0
+    let mode;
+    //variables for mean
+    let total = 0
+    let mean;
+ //determine which number is repeated the most
+ //go through array create a key in a new object to hold the times it sees the number
+    //if number exists in the object then increase the count by 1
+    //check which key as the highest number
+    for (let i = 0; i < array.length; i++) {
+        if(!modeObj[array[i]]) {
+            modeObj[array[i]] = 0;
+        } 
+        modeObj[array[i]] += 1 
+    }
+    for(keys in modeObj) {
+        if(modeObj[keys] > maxFreq) {
+            maxFreq = modeObj[keys]
+            mode = keys
+        }
+    }
+    mode;
+        //function to find mean
+    for (let i = 0; i < array.length; i++) {
+            total += array[i]
+    }
+        mean = Math.floor(total/2)
+    if (mean === mode) {
+        return true;
+    } else return false;
 }
+
+
+
+let modeMeanArray = [1, 3, 3, 4, 5]
+modemean(modeMeanArray);
 
 module.exports = modemean;
