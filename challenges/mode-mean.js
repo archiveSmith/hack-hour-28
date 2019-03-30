13ec5729c798507404af2209697bc7fc60c2fce5
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-03-30 09:35:00 -0700

    completed modemean hack hour challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,28 @@
 
 
 function modemean(array) {
-
+    let countObj = {}
+    let sum = 0;
+    let mode = null;
+    for (let i = 0; i < array.length; i += 1) {
+        if (!countObj[array[i]]) {
+            countObj[array[i]] = 1;
+        } else {
+            countObj[array[i]] += 1;
+            if (countObj[array[i]] >= mode && array[i] > mode) {
+                mode = array[i];
+            }
+        }
+        sum += array[i]
+    }
+    let mean = Math.floor(sum / array.length);
+    // console.log(mode)
+    // console.log(mean)
+    return mean === mode;
 }
+
+console.log(modemean([1,2,3,4,5,6,7,4]))
+console.log(modemean([1,1]))
+console.log(modemean([2,2,1,1]))
 
 module.exports = modemean;
