b56b617c697c85d309aef758510c6425f8e69c7b
Johnny Lim <bluejayl@ucla.edu>
2019-03-30 09:50:52 -0700

    completed mode-mean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -12,6 +12,50 @@
 
 function modemean(array) {
 
+  const dict = {};
+ 
+  for (let i = 0; i < array.length; i++) {
+    if (dict[array[i]] === undefined) {
+      dict[array[i]] = 1;
+    } else {
+      dict[array[i]]++;
+    }
+  }
+
+  let value = 0;
+  for (let prop in dict) {
+    
+    if (dict[prop] > value) {
+      value = dict[prop];
+    }
+  }
+  
+  let mode
+
+  for(let prop in dict) {
+    if (value === dict[prop]) {
+      mode = prop
+    }
+  }
+  
+  const sum = array.reduce((acc, curr) => {
+    return acc + curr;
+  })
+  
+  const mean = Math.floor(sum/2) 
+  const modeNumber = parseInt(mode);
+
+  if (modeNumber === mean) {
+    return true;
+  }
+  return false;
 }
+
+
+// const arr = [2,2] ;
+// const arr2 = [1,1,1];
+
+// console.log(modemean(arr));
+// console.log(modemean(arr2));
 
 module.exports = modemean;
