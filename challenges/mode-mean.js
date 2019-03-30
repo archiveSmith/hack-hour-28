5205eb4104c615a3374e708e1c9b6bee9bebb16f
natlynp <natlynp@gmail.com>
2019-03-30 09:47:29 -0700

    completed modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,38 @@
 
 
 function modemean(array) {
+    //find average of array (sum/array length)
+    let mean = array.reduce((acc, current) => {
+        return acc += current;
+    }, 0)
+    mean = Math.floor(mean/array.length);
+    
+    //find mode by keeping track of how many times the number appears in array
+    let modeObj = {}
+    array.forEach(num => {
+        if (!modeObj[num]) {
+            modeObj[num] = 0;
+        } 
+        modeObj[num]++
+    }) 
 
+    //initialize variable to track the number of times that number appeared
+    let count = 0;
+    //initialize variable for mode
+    let mode;
+    //loops through object and checks if the number count is greater than the current count
+    for (let key in modeObj) {
+        if (modeObj[key] >= count) {
+            //assign/update count when the count is greater than current
+            count = modeObj[key];
+            //assign the mode to that key
+            mode = key;
+        }
+    }
+
+    if (mean === mode) return true;
+    else return false
 }
+
 
 module.exports = modemean;
