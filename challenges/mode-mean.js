0bac84a04fe180bace56abe818b425fa0dc950d0
Keith <keith.avila@gmail.com>
2019-03-30 10:15:13 -0700

    mode mean complete

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,34 @@
 
 
 function modemean(array) {
-
+  let newArr = array.sort();
+  let avgSum = 0;
+  let modeObj = {};
+  for (let i = 0; i < newArr.length; i++) {
+    avgSum += newArr[i];
+    if (!modeObj[newArr[i]]) {
+      modeObj[newArr[i]] = 1;
+    }
+    else modeObj[newArr[i]]++;
+  }
+  let modeHold = 0;
+  let maxFreq = 0;
+  for (const key in modeObj) {
+    if (modeObj[key] > maxFreq) {
+      maxFreq = modeObj[key];
+      modeHold = key;
+    };
+    if (modeObj[key] === maxFreq && key > modeHold) {
+      maxFreq = modeObj[key];
+      modeHold = key;
+    }
+  }
+  console.log(modeHold);
+  let avg = (Math.floor(avgSum/array.length));
+  console.log(avg);
+  console.log(modeHold == avg)
+  return modeHold == Math.floor(avgSum/array.length);
 }
+console.log(modemean([4, 4, 4]));
 
 module.exports = modemean;
