386af28e361bcd4a46d16783facaebb6cc5073b3
Mulderink1 <Mulderink1@gmail.com>
2019-03-30 10:18:47 -0700

    HackHour 3-30

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,27 @@
 
 
 function modemean(array) {
-
+  let allNumsAdded = 0;
+  const modeObj = {};
+  for (let i = 0; i < array.length; i += 1) {
+    allNumsAdded += array[i];
+    if (!modeObj[array[i]]) modeObj[array[i]] = 1;
+    else  modeObj[array[i]] += 1;
+  }
+  mean = Math.floor(allNumsAdded / array.length)
+  let max = 0;
+  let mode = 0;
+  const objKeys = Object.keys(modeObj)
+  for (let i = 0; i < objKeys.length; i += 1) {
+    const keyvalue = modeObj[objKeys[i]];
+    if (max < keyvalue) {
+      max = keyvalue;
+      mode = objKeys[i];
+    }
+  }
+  return (mode == mean) ? true : false;
 }
 
 module.exports = modemean;
+
+console.log(modemean([1, 2]))
