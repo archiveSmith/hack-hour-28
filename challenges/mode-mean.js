a474bb9af4061a77c90398ae1118d05f97fb9778
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-03-30 09:34:42 -0700

    complete mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,8 +10,44 @@
  */
 
 
-function modemean(array) {
+const modemean = (array) => {
+let mean = array.reduce((total, next) => {
+    total = total+next;
+    return total;
+})/array.length;
+
+let mode = {};
+
+for(let i = 0; i < array.length; i++){
+    if(mode[array[i]] === undefined){
+        mode[array[i]] = 0;
+    } mode[array[i]]++;
+}
+
+
+
+let max = 0;
+let modeVal = null;
+
+for(let key in mode){
+    if(mode[key] === max){
+        max = mode[key];
+        modeVal = parseInt(key);
+    } else if(mode[key] > max){
+        max = mode[key];
+        modeVal = parseInt(key);
+    }
+}
+
+console.log(modeVal)
+
+if(Math.floor(mean) === mode) {
+    return true;
+  } else 
+    return false;
 
 }
+
+console.log(modemean([1,2,3,4,4,3,3]))
 
 module.exports = modemean;
