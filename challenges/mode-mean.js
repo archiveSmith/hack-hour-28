c7ec54ef9f7db1c931f8f4981959e75eff3d01b1
Timothy Johnson <Timmy@Timothys-MacBook-Pro.local>
2019-03-30 09:38:47 -0700

    Done.

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,48 @@
 
 
 function modemean(array) {
+  let newObj = {};
+  for(let i = 0; i < array.length; i++)
+  {
+    //Find the num of occurrances.
+    if(newObj[array[i]] === undefined)
+    {
+      newObj[array[i]] = 1;
+    }
+    else {
+      newObj[array[i]]++;
+    }
+  }
+
+ //Find number that occurs the most(the mode).
+  let biggest = 0;
+
+  for(let x in newObj)
+  {
+    if(newObj[x] > biggest)
+    {
+      biggest = x;
+    }
+  }
+
+
+  //Now find the mean.
+  let count = 0;
+  for(let i = 0; i < array.length; i++)
+  {
+    count += array[i];
+  }
+  let mean = Math.floor(count/array.length);
+
+
+  if(biggest === mean)
+  {
+    return true;
+  }
+  return false;
 
 }
+
+console.log(modemean([1,2,2,5,7,1,2]))
 
 module.exports = modemean;
