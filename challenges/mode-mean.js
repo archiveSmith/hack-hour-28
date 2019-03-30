0d150826d57483944bb9d21b8994bdcd3f927efb
Steven <stevj17@gmail.com>
2019-03-30 10:09:15 -0700

    submitted

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,53 @@
  *
  */
 
+ let sample = [2, 5, 7, 8, 5, 1, 9, 9, 2];
+
 
 function modemean(array) {
+    const sum = array.reduce((acc, curr) => {
+        return acc + curr;
+    })
 
+    let mean = Math.floor(sum / array.length);
+    
+    let sorted = array.sort();
+    let numMap = {};
+    for(var i = 0; i < array.length; i++){
+        if(numMap[array[i]] === undefined){
+            numMap[array[i]] = 0;
+        }        
+            numMap[array[i]] += 1;
+    }
+
+
+    for(let keys in numMap){
+        let value = numMap[keys];
+        console.log(value);
+     
+       // for each key, if the value 
+
+       console.log(value);
+       console.log(keys)
+    }
+    let keyVals = Object.entries(numMap);
+    console.log(keyVals);
+    
+   
+    let max = Math.max(...keyVals);
+
+    console.log(max);
+    console.log(numMap);
+
+    if(max === mean){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+
+console.log(modemean(sample));
+
 
 module.exports = modemean;
