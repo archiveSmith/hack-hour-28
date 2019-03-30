277a50344ee93e74d3725d91c41136aade40551f
alyviam <alyvialmoss@gmail.com>
2019-03-30 09:58:05 -0700

    completed hack-hour challenge

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,6 +1,14 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
+
+ //console.log('staircase') where:
+    //staircase consists of *
+    //staircase height 1 <= N <= 100;
+        //N is never less than 1 and never greater than 100
+    //climb from left to right
+        //decrementing loop?
+    //last line no leading trailing spaces
  
  For example:     
  drawStairs(6) ->          
@@ -14,8 +22,14 @@
 */
 
 function drawStairs(n) {
-
+    for (i = 1; i <= n; i++) {
+        console.log(i)
+        console.log(' '.repeat(n - i) + '*'.repeat(i))
+    }
 }
 
+
+
+console.log(drawStairs(6));
 
 module.exports = drawStairs;
--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,32 @@
 
 
 function modemean(array) {
+	if (array.length === 1) {
+		return true;
+	}
 
+	//mean: 
+	const total = array.reduce(function (acc, current) {
+		acc = acc + current;
+		return acc;
+	})
+
+	const mean = Math.floor(total / array.length);
+
+	//mode:
+	const mode = array.filter((el, i) => {
+		return array.indexOf(el) !== i;
+	}).reduce((acc, current) => Math.max(acc, current));
+
+	//are mode and mean equal?
+	return mode === mean ? true : false;
 }
+
+console.log(modemean([1, 2, 3, 2, 3]));
+console.log(modemean([1, 1, 1]));
+console.log(modemean([1, 1, 1, 2, 3]));
+console.log(modemean([2, 2, 6, 8]));
+console.log(modemean([5]));
+
 
 module.exports = modemean;
