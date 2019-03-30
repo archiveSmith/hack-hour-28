d1731a840baba8c95c151cc5e2c42d5648b29b77
Alex Park <apark0720@gmail.com>
2019-03-30 09:31:21 -0700

    complete modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,27 @@
 
 
 function modemean(array) {
+  let count = {};
+  let sum = 0
+  let mode = 0
 
+  array.forEach(el => {
+    sum += el;
+
+    if (count[el]) {
+      count[el] += 1
+      if (count[el] > mode) mode = el;
+     } else {
+       count[el] = 1;
+     }
+  });
+
+  let mean = Math.floor(sum / (array.length))
+  
+  return mode === mean;
 }
+
+let arr = [6,4,5,5];
+console.log(modemean(arr));
 
 module.exports = modemean;
