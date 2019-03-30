d382e950cfc75611b4639cb68b17f0e4495a0e31
Sam Ryoo <samryoo@gmail.com>
2019-03-30 10:00:41 -0700

    finished

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,27 @@
 
 
 function modemean(array) {
+    let arrSum = array.reduce(function(a,b){
+          return a + b
+        }, 0);
+    let avg = Math.floor(arrSum/array.length);
 
+    let countObj = {};
+    for (let i =0; i<array.length; i++) {
+      if (!countObj[array[i]]) countObj[array[i]] = 1
+      else countObj[array[i]]++;
+    }
+
+    let greatestFreq = 0;
+    let mode;
+    for(var prop in countObj){
+        if(countObj[prop] > greatestFreq){
+            greatestFreq = countObj[prop];
+            mode = prop;
+        }
+    }
+    if (mode === avg) return true;
+    return false;
 }
 
 module.exports = modemean;
