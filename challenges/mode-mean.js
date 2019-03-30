7371c2f3191f5195e32b51e5d0551fa75610d322
chetbaby <cbaby@CBMB.local>
2019-03-30 09:39:44 -0700

    compleated mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,30 @@
  *
  */
 
-
 function modemean(array) {
+  // OUT: Boolean
+  //   in: array of numbers
+  // calculate the mean (return Math.floor(mean))
+  const arrMean = mean(array); //?
+  // calculatethe mode
+  const arrMode = mode(array); //?
+  return arrMean == arrMode;
+}
 
+function mean(arr) {
+  let total = arr.reduce((acc, curr) => {
+    return (acc = acc + curr);
+  });
+  return Math.floor(total / arr.length);
+}
+
+function mode(arr) {
+  let modeCount = {};
+  arr.forEach((element, i) => {
+    modeCount[element] ? (modeCount[element] += 1) : (modeCount[element] = 1);
+  });
+  let countArr = Object.entries(modeCount).sort((a, b) => b[1] - a[1]); //?
+  return countArr[0][0]; //?
 }
 
 module.exports = modemean;
