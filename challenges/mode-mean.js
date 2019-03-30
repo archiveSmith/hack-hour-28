3ee124a14f86fe1bf70aeb606d430c3c323cd526
mapleseventh <mapleseventh@gmail.com>
2019-03-30 09:40:15 -0700

    'mode-mean-1'

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
+    let count = {};
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        let current = array[i];
+        sum += current;
+        if(!count[current]) count[current] = 1;
+        else count[current]++
+    }
+
+    let mean = sum/array.length;
+    let keyArr = Object.keys(count);
+    mode = keyArr.reduce(function(acc, cur){
+        if(count[acc] > count[cur]) return acc
+        else return cur
+    })
+
+    console.log(count);
+    console.log(mode);
+
+    console.log(mean);
+    return mean === mode
 
 }
+
+let arr = [7,1,6,9,11,5,5,9,9,7,7,7,7]
+console.log(modemean(arr)) 
 
 module.exports = modemean;
