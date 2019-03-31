73f76a32f03142730b62d2cfb3298146655da0ba
Karen Pinilla <pinillakaren11@gmail.com>
2019-03-30 21:53:08 -0700

    finished mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,17 +11,20 @@
 
 
 function modemean(array) {
+    if (calculateMean(array) === calculateMode(array)) {
+        return true;
+    } else return false;
+}
+
+function calculateMode (array) {
     //variables for mode
     let modeObj = {}
     let maxFreq = 0
     let mode;
-    //variables for mean
-    let total = 0
-    let mean;
- //determine which number is repeated the most
- //go through array create a key in a new object to hold the times it sees the number
-    //if number exists in the object then increase the count by 1
-    //check which key as the highest number
+//determine which number is repeated the most
+//go through array create a key in a new object to hold the times it sees the number
+//if number exists in the object then increase the count by 1
+//check which key as the highest number
     for (let i = 0; i < array.length; i++) {
         if(!modeObj[array[i]]) {
             modeObj[array[i]] = 0;
@@ -32,22 +35,26 @@ function modemean(array) {
         if(modeObj[keys] > maxFreq) {
             maxFreq = modeObj[keys]
             mode = keys
-        }
+        } 
     }
-    mode;
-        //function to find mean
-    for (let i = 0; i < array.length; i++) {
-            total += array[i]
-    }
-        mean = Math.floor(total/2)
-    if (mean === mode) {
-        return true;
-    } else return false;
+    return mode;
 }
 
+function calculateMean(array) {
+        //variables for mean
+        let total = 0
+        let mean;
+            //function to find mean
+        for (let i = 0; i < array.length; i++) {
+                total += array[i]
+        }
+         mean = Math.floor(total/2)
+         return mean
+}
 
-
-let modeMeanArray = [1, 3, 3, 4, 5]
-modemean(modeMeanArray);
+let modeMeanArray = [1, 2, 8, 8, 1];
+console.log(calculateMode(modeMeanArray));
+console.log(calculateMean(modeMeanArray));
+console.log(modemean(modeMeanArray));
 
 module.exports = modemean;
