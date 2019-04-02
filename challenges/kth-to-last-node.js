cd3b6c06bf243a9db44ad990df0368d69c0ef9e5
RadicalRoy <rolandquintana1991@gmail.com>
2019-04-02 09:30:16 -0700

    your challenge, if you choose to accept it, is to ensure that the kth to last value in linkedlist is returned

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -8,10 +8,6 @@
  * const d = new Node('D');
  * const e = new Node('E');
  *
- * a.next = b;
- * b.next = c;
- * c.next = d;
- * d.next = e;
  *
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
@@ -23,6 +19,40 @@ function Node(val) {
 
 function kthToLastNode(k, head) {
 
-}
+  // find length
+  let current = head;
+  let length = 0;
+  while(current) {
+    length++;
+    current = current.next;
+  }
 
+  if (k > length) return undefined;
+
+  let counter = 0;
+
+  //now we know the position of the kth to last, so loop again and return it
+  //reset current and loop again
+  current = head;
+  while(current) {
+    if(counter++ == length - k) return current.value;
+    current = current.next;
+  }
+
+
+}
+/*
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(3, a));
+*/
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
