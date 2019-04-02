719fb19d1b6eb4312f9f5aed664ceb9ef31ebb68
j218 <jaymlim93@gmail.com>
2019-04-02 10:02:47 -0700

    did not finish

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -15,14 +15,62 @@
  *
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
 
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
-
+LinkedList.prototype.add = function(val) {
+  const newNode = new Node(val);
+  if(!this.head.value) {
+    this.head = newNode;
+    newNode.next = null;
+  } else {
+    this.head.next = newNode;
+  }
+  this.tail = newNode;
 }
+
+function kthToLastNode(k, head) {
+  // we have the head of the linked list;
+  // find the tail of the linked list 
+  // iterate backwards k times to get the value
+  let current = this.head;
+  let counter = [];
+  // if current.next equals null then we hit the last tail node.
+  // counter to see how many nodes we need to go back from tail node.
+  while(current.next !== null) {
+    if(current.next) {
+      current = current.next;
+    } 
+    counter.push(current.value);
+  }
+  // check array and from the last element subtract k. That's the index that gives you the value.
+  const kthVal = (counter.length - 1) - k;
+  return counter[kthVal];
+
+  // return the value of the kth node. 
+}
+const a = new Node('A');
+console.log(a);
+
+const b = new Node('B');
+console.log(b);
+
+const c = new Node('C');
+console.log(c);
+
+const d = new Node('D');
+console.log(d);
+
+const e = new Node('E');
+console.log(e);
+
+
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
