319dbc91beb9aba0a131d85d040fcb6db40c47f0
alyviam <alyvialmoss@gmail.com>
2019-04-02 10:05:38 -0700

    Alyvia Moss' completed hack-hour challenge: kth-to-last-node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -16,13 +16,62 @@
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
 
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
+//write a function that takes two parameters
+//arguments are an integer and the head of a singly linked list
+//function returns the value on the kth to last node in the list 
+//"2nd to last" - but make it general - "kth to last"
+
 function kthToLastNode(k, head) {
+  console.log(head.value)
+
+
+
+
+  //take care of edge cases
+  if (head === undefined) {
+    return undefined;
+  } else if (k === undefined) {
+    return head;
+  };
+
+
+  let trackObj = {};
+  let count = 0;
+  let current = head;
+
+  while (current) {
+    trackObj[count] = current.value;
+    count++
+    current = current.next;
+  }
+
+
+  let num = Object.keys(trackObj).length - (k)
+  console.log(num)
+  console.log(trackObj)
+
+  return trackObj[num]
 
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+//console.log(kthToLastNode(2, a))
+//console.log(kthToLastNode(3, a))
+//console.log(kthToLastNode(1, a))
+//console.log(kthToLastNode(5, a))
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };
