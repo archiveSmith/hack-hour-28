808c74a32226e6cfdd78ef848e7478f76be26559
Karen Pinilla <pinillakaren11@gmail.com>
2019-04-02 10:33:15 -0700

    finished kth-to-last

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -16,13 +16,59 @@
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
 
+ //create a linkedList
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
+let linked = new LinkedList;
+
+
+
 function kthToLastNode(k, head) {
+let valueArray = [head.value];
+let nextNode = head.next;
+  //check to see if head.next exists 
+  while (nextNode != null) {
+    // if (this.head.next = null) {
+    //   let tail = this.head.next;
+    // }
+    valueArray.push(nextNode.value);
+    nextNode = nextNode.next;
+  }
+  ///from end of the length of array subtract k and return the value of index
+  return valueArray[valueArray.length - k];
 
 }
+
+
+
+//push to Linkedlist
+LinkedList.prototype.push = function(value) {
+  let newNode = new Node(value);
+  //if statement checking if next is null
+  if (!this.head) {
+    //create a node
+    //set the head and tail to the new node
+    this.head = newNode;
+  } else {
+    this.tail.next = newNode;
+  }
+  this.tail = newNode;
+};
+
+linked.push('A')
+linked.push('B')
+linked.push('C')
+linked.push('D')
+linked.push('E')
+
+console.log(linked)
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
