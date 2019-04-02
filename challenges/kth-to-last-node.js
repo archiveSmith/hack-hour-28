00b5314b80fff550e5eaae9ebf4685b0242186d1
chetbaby <cbaby@CBMB.local>
2019-04-02 09:53:11 -0700

    it's working

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,23 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
-
+function kthToLastNode(k, head, values = []) {
+  // console.log(head.value, head.next);
+  if (k === 1 && head.next === null) {
+    return head.value;
+  } else if (head.next === null) {
+    return values[values.length - k + 1];
+  } else values.push(head.value);
+  let nextNode = head.next;
+  return kthToLastNode(k, nextNode, values);
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+let newNode = new Node(1);
+newNode.next = new Node(2);
+newNode.next.next = new Node(3);
+newNode.next.next.next = new Node(4);
+newNode;
+
+kthToLastNode(1, newNode); //?
+
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };
