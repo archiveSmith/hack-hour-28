d71432617598ac3e24cc12a7665a737d432be5ce
Keith <keith.avila@gmail.com>
2019-04-02 09:45:34 -0700

    kth complete

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,33 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
 
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+function kthToLastNode(k, head) {
+  if (k === 0) return 'For the last node, enter 1, not 0';
+  let nodeCount = 1;
+  let headCopy = head;
+  while (headCopy.next) { 
+    nodeCount++;
+    headCopy = headCopy.next;
+  }
+
+  let kth;
+  for (let i = 0; i <= nodeCount - k; i++) {
+    kth = head.value;
+    head = head.next;
+  }
+  return kth; 
 }
+console.log(kthToLastNode(2, a));
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
