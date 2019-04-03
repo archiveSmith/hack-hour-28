04e7a5de5eeb19acf9fb886bed7759f66345fdfd
Mulderink1 <Mulderink1@gmail.com>
2019-04-03 09:49:22 -0700

    Finished HackHour 4-3-19.

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,16 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, total = base, count = 1) {
+  if (power === 0) return 1;
+  if (count >= power) return total;
+  total = total * base;
+return pow(base, power, total, count += 1);
 }
 
 module.exports = pow;
+
+console.log(pow(0, 3)) // --> 0 
+console.log(pow(3, 0)) // --> 1 
+console.log(pow(3, 3)) // --> 27
+console.log(pow(5, 8)) // --> 390625
