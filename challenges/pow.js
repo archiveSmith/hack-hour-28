92624c523c8793fda6a773203d5261cf49ae77ed
natlynp <natlynp@gmail.com>
2019-04-03 09:59:24 -0700

    pow edited

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,9 +3,9 @@
  */
 
 function pow(base, power) {
+    if (power === 0) return 1;
     if (power === 1) return base;
-    power = power -1
-    return base * pow(base, power)
+    return base * pow(base, power -1)
 }
 
 module.exports = pow;
