a48fa9372752932b32ee2c0978669d4dfc58ebf3
mapleseventh <mapleseventh@gmail.com>
2019-04-03 09:44:26 -0700

    THE POWER OF POWER

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,6 +4,23 @@
 
 function pow(base, power) {
 
+    if(power === 1) return base;
+    if(power !== 1) return base *= pow(base, power - 1);
+
+    //return pow(base, power - 1)
+
+    // const foundation = base;
+    // let output;
+    // function multiply(b) {
+    //     output = b * base;
+        
+    // }
 }
 
+console.log(pow(2,1)) // expect 3
+console.log(pow(2,2)) // expect 9
+console.log(pow(2,3)) // expect 27
+console.log(pow(2,4)) // expect 81
+console.log(pow(2,5)) // expect 81
+console.log(pow(2,6)) // expect 81
 module.exports = pow;
