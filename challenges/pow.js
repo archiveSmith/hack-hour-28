6b69ef01b0083467d614514d4c24231c21b817aa
Jonathan Peralez <jonathanperalez@Jonathans-MacBook-Pro.local>
2019-04-03 10:00:30 -0700

    kinda working

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,30 @@
  * Use recursion!
  */
 
-function pow(base, power) {
+let baseNum;
+let ranOnce = false;
+let isNegative = false;
+let negativePow = false;
 
+function pow(base, power) {
+  // base case
+  if (base < 0) {
+    isNegative = true;
+  }
+  // negative power
+  if (power < 0) {
+    negativePow = true;
+  }
+  if (power === 0) return 1;
+  if (power === 1) {
+    return isNegative ? Math.abs(base) * -1 : Math.abs(base);
+  }
+  if (!ranOnce) {
+    baseNum = base;
+    ranOnce = true;
+  }
+  return pow(base * baseNum, power - 1);
 }
+
 
 module.exports = pow;
