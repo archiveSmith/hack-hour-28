a2adcf85da0aea2ba0976a1b74c94b1d89f5f665
j218 <jaymlim93@gmail.com>
2019-04-03 09:35:01 -0700

    finished with pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,19 @@
  */
 
 function pow(base, power) {
+    if(typeof base !== 'number') return 'not valid input!'
+    
+    if(base === 0) return 0;
 
+    if(power === 1) return base;
+
+    return base * pow(base, power - 1);
 }
+console.log(pow('hello', 2))
+console.log(pow(0, 2));
+console.log(pow(4,1));
+console.log(pow(2,3));
+console.log(pow(2,5));
+console.log(pow(3, 3));
 
 module.exports = pow;
