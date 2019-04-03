7bfb088c2ae6a81551d6ce3fd350ebc9acf5d034
Steven <stevj17@gmail.com>
2019-04-03 09:48:14 -0700

    hackhour4.3

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,14 @@
  */
 
 function pow(base, power) {
-
+    // For cases dealing with 0
+    if(power === 0) return 1;
+    // --------------------------
+    // base ** power = number times itself n number of times e.g. 5 ** 4 = 5 * 5 * 5 * 5
+    else{
+    return base * pow(base, power - 1);
+    }
 }
+console.log(pow(5, 4));
 
 module.exports = pow;
