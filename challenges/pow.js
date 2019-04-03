e22a82b58cec74ec88f0142bf4db961f4a646166
RadicalRoy <rolandquintana1991@gmail.com>
2019-04-03 09:27:56 -0700

    finished pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,15 @@
  */
 
 function pow(base, power) {
+  
+  if(power < 0) return undefined;
+
+  if (power === 0) return 1;
+
+  return base*pow(base, power - 1);
 
 }
+
+console.log(pow(7, -1));
 
 module.exports = pow;
