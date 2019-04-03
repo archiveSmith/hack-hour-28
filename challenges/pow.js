49baecf01b2523d2ee0f47029ae20f39087da35a
Karen Pinilla <pinillakaren11@gmail.com>
2019-04-03 09:58:14 -0700

    finished pow hack hour

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,22 @@
  * Use recursion!
  */
 
-function pow(base, power) {
+//(2, 3) 2 * 2 = 4 * 2
 
+function pow(base, power, result = base) {
+//if statement to check when it reaches a certain number then return the answer
+//as the power increases base stays the same
+//base * base = result * base 
+    if (power === 1) return result;
+    if (power === 0) return 0;
+    result = base * result;
+    power --
+    return pow(base, power, result)
 }
+
+
+
+
+console.log(pow(2, 1))
 
 module.exports = pow;
