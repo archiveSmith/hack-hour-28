4a3e7239e98b706164a74faf3d7895a3f8bd13bc
alyviam <alyvialmoss@gmail.com>
2019-04-03 09:54:52 -0700

    Alyvia's completed hack hour challenge pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,6 +4,19 @@
 
 function pow(base, power) {
 
+	let tracker = power;
+	let value = base;
+	if (tracker === 1) {
+		return value
+	} else if (tracker === 0) {
+		return 1;
+	}
+	tracker -= 1
+	return value * pow(base, tracker)
 }
 
+console.log(pow(2, 3))
+console.log(pow(6, 4))
+console.log(pow(2, 1))
+console.log(pow(2, 0))
 module.exports = pow;
