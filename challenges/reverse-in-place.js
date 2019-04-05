c90d57fd6de2243e2589b6e662091b9798591789
chetbaby <cbaby@CBMB.local>
2019-04-05 09:29:25 -0700

    used unshift

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /**
  * Write a function to reverse an array in place
  *
@@ -14,7 +14,10 @@
  */
 
 function reverseInPlace(array) {
-
+  let reversed = [array[0]];
+  for (let i = 1; i < array.length; i += 1) {
+    reversed.unshift(array[i]);
+  }
+  return reversed;
 }
-
 module.exports = reverseInPlace;
