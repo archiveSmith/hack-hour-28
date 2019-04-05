187dadc3970655744676895141b1e4ef35ef278f
Keith <keith.avila@gmail.com>
2019-04-05 09:41:53 -0700

    reverse-in-place complete

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,11 @@
  */
 
 function reverseInPlace(array) {
-
+  for (let i = 0; i < Math.floor(array.length/2); i++) {
+    [array[i], array[array.length - 1 - i]] = [array[array.length - 1 - i], array[i]];
+  }
+  return array;
 }
 
+console.log(reverseInPlace(['k', 'e',]));
 module.exports = reverseInPlace;
