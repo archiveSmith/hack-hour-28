b712e8dc5fa9e7e1ff57446698dcd674c2db61d4
j218 <jaymlim93@gmail.com>
2019-04-06 09:54:53 -0700

    done with string rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,34 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+  // rotate the string and see if at specific index it is found. 
+  // so use substring func to check if each character is available 
+    // if not return false.
+  // create an array of characters. 
+    // find the index of first character in second string and from that point iterate through and make checks
+  // const index = isSubstring(s2, s1[0]); // 3
+  // console.log(isSubString(s1, s2))
+  if(s1.length !== s2.length) {
+    return false;
+  } else {
+    const index = s2.indexOf(s1[0]);
+    let halfS2 = s2.substring(index, s2.length);
+    let otherHalfS2 = s2.substring(0, index);
+    const newS2 = halfS2 + otherHalfS2;
+    // console.log(newS2);
+    // console.log(index);
+    for(let i = 0; i < s1.length; i += 1) {
+      if(s1[i] !== newS2[i]) {
+        return false;
+    }
+  }
+    return true;
+ }
 }
+  
+console.log(stringRotation("hello", "hello"))
+console.log(stringRotation("hello", "llohe"))
+console.log(stringRotation("hello", "ollhe"))
+console.log(stringRotation("hello", "he"))
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
