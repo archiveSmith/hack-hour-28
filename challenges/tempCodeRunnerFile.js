afc4d6b9aa9256313506e87329f205b4f1c887ef
Alex Park <apark0720@gmail.com>
2019-04-06 09:51:54 -0700

    finish string rotation

--- challenges/tempCodeRunnerFile.js
+++ challenges/tempCodeRunnerFile.js
@@ -1,1 +1,21 @@
-drawStairs(6);
+function isSubstring(s1, s2) {
+  return s1.indexOf(s2) >= 0;
+}
+
+function stringRotation(s1, s2) {
+  if (isSubstring(s1, s2)) {
+    let temp = s2.split('');
+
+    for (let i = 0; i < temp.length; i++) {
+      temp.push(temp.unshift(temp[0]));
+      if (s1 === temp.join('')) return true;
+    }
+  }
+
+  return false;
+}
+
+module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+
+
+console.log(stringRotation("hello", "hello"));
