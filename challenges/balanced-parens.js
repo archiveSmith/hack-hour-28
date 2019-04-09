77a40faf64f004b3d792ff25a89f82e33c94b35f
Mulderink1 <Mulderink1@gmail.com>
2019-04-09 09:43:32 -0700

    4-9-19 hackhour

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,19 @@
  */
 
 function balancedParens(input){
-
+  let result = true;
+  let strArr = input.split(" ")
+  for (let i = 0; i < strArr.length; i += 1) {
+    if (strArr[i].includes('[', ']', '{', '}', '(', ')')) {
+      if (strArr[i] === '[' && strArr[i].includes(']') === false) result = false;
+      if (strArr[i] === ']' && strArr[i].includes('[') === false) result = false;
+      if (strArr[i] === '{' && strArr[i].includes('}') === false) result = false;
+      if (strArr[i] === '}' && strArr[i].includes('{') === false) result = false;
+      if (strArr[i] === '(' && strArr[i].includes(')') === false) result = false;
+      if (strArr[i] === ')' && strArr[i].includes('(') === false) result = false;
+    }
+  }
+  return result;
 }
 
 module.exports = balancedParens;
