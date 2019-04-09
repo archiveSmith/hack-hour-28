7b93f6acebeebd9f08130fac4d3e489e0416ccfa
Steven <stevj17@gmail.com>
2019-04-09 09:47:50 -0700

    submission

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -23,9 +23,31 @@
  *
  *
  */
+const open = new Set(['{', '[', '('])
+const close = new Set(['}', ']', ')'])
+const pairs = { '{':'}', '[':']', '(': ')' }
 
-function balancedParens(input){
+function balancedParens(input, stack = []){
+    // () , [].  {}
+    const next = input[0];
 
+    if(input === '') {
+      return !stack.length
+    } else if(close.has(next) && pairs[stack.pop()] !== next) {
+      return false;
+    } else if(open.has(next)) {
+      stack.push(next)
+    } else {
+    }
+    return balancedParens(input.slice(1), stack)
 }
 
+   
+    
+
+console.log(balancedParens('[({})]'));
+console.log(balancedParens('(())'));
+console.log(balancedParens('[](){}')); 
+console.log(balancedParens('[({})]'));   
+balancedParens('[(]{)}');
 module.exports = balancedParens;
