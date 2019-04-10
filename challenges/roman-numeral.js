a7206a8f5b93d442b8286083fec817598bb6abe6
chetbaby <cbaby@CBMB.local>
2019-04-10 09:58:48 -0700

    solved with nested arr lookup

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,28 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
+  const romanNums = [
+    [, "M", "MM", "MMM", "MV", "V", "VM", "VMM", "VMMM", "VV"],
+    [, "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+    [, "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    [, "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+  ];
 
+  let numStr = ("" + n).split("");
+  let romanStr = "";
+  numStr;
+  for (let i = numStr.length - 1; i >= 0; i -= 1) {
+    let romanIndex = parseInt(numStr[i]);
+    let concatRoman = romanNums[i][romanIndex] || "";
+    romanStr = concatRoman + romanStr;
+  }
+  return romanStr;
 }
+
+romanNumeral(8580); //?
 
 module.exports = romanNumeral;
