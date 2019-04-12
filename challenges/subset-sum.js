2a1d3df827f5c1d6f871dd591ad08cf5c8b90ab6
chetbaby <cbaby@CBMB.local>
2019-04-12 09:57:25 -0700

    didn't solve

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -1,7 +1,7 @@
 /* You are given an array of integers and a target number. Write a function that returns true if
  * there is a subset of the array that sums up to the target and returns false otherwise. A subset
  * can be any size and the elements do not have to appear consecutively in the array.
- * 
+ *
  * subsetSum([3, 7, 4, 2], 5) - > true, 3 + 2 = 5
  * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
  * subsetSum([8, 2, 4, 12], 13) -> false
@@ -9,7 +9,10 @@
  */
 
 function subsetSum(array, target) {
-
+  const subtractedVals = array.map(el => target - el); //?
+  if (subtractedVals.includes.apply(subtractedVals, array)) return true;
 }
+
+subsetSum([3, 7, 4, 2], 5); //?
 
 module.exports = subsetSum;
