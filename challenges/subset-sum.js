733aba15748d1a73c9c71c0c71611a6cc69c1807
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-04-12 10:07:24 -0700

    completed subset sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -10,6 +10,44 @@
 
 function subsetSum(array, target) {
 
+    let sumExists = false;
+
+    array.forEach(el => {
+    
+        for (let i = 1; i < array.length; i += 1) {
+
+            if (el === target) {
+                sumExists = true;
+            }
+
+            if ((el + array[i]) === target) {
+                console.log(el)
+                console.log(array[i])
+                console.log(el + array[i])
+                sumExists = true;
+            }
+
+            for (let j = 2; j < array.length; j += 1) {
+                if ((el + array[i] + array[j]) === target) {
+                    console.log(el + array[i] + array[j])
+                    sumExists = true;
+                }
+                for (let k = 3; k < array.length; k += 1) {
+                    if ((el + array[i] + array[j] + array[k]) === target) {
+                        sumExists = true;
+                    }
+                }
+            }
+        }
+    })
+    return sumExists;
 }
+
+console.log(subsetSum([3, 7, 4, 2], 5))
+console.log(subsetSum([3, 34, 4, 12, 5, 12], 32))
+console.log(subsetSum([8, 2, 4, 12], 13))
+console.log(subsetSum([8, -2, 1, -3], 6))
+
+
 
 module.exports = subsetSum;
