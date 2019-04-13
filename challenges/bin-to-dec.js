8a46e2202eab13e3facad177a8feb0c2f8826112
alyviam <alyvialmoss@gmail.com>
2019-04-13 09:35:09 -0700

    Alyvia's solution to hack-hour bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,32 @@
  */
 
 function binToDec(binary) {
-
+	if (typeof binary !== "string") {
+		const inputToStr = binary.toString();
+		return parseInt(inputToStr, 2);
+	}
+	return parseInt(binary, 2);
 }
+
+const hexaToDec = (string => {
+	if (typeof string !== "string") {
+		const inputToStr = string.toString();
+		return parseInt(inputToStr, 2);
+	}
+	return parseInt(string, 16)
+})
+
+//console.log(binToDec(1))
+console.log(binToDec('0'))   //-> 0
+console.log(binToDec('11'))  //-> 3
+console.log(binToDec('100')) //-> 4
+console.log(binToDec('101')) //-> 5
+console.log(binToDec('0101'))
+
+console.log(hexaToDec('0'))   //-> 0
+console.log(hexaToDec('11'))  //-> 3
+console.log(hexaToDec('100')) //-> 4
+console.log(hexaToDec('101')) //-> 5
+console.log(hexaToDec('0101'))
 
 module.exports = binToDec;
