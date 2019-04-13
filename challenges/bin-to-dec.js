e189a05f28561d36488ba29bfdb493e8a4943bd6
CBABY! <cbaby@CBABYs-MacBook-Pro.local>
2019-04-13 09:58:41 -0700

    not sure if it works

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -3,7 +3,7 @@
  * Write a function that converts the binary string to a decimal number
  *
  * Example:
-* 	binToDec('0')   -> 0
+ * 	binToDec('0')   -> 0
  * 	binToDec('11')  -> 3
  * 	binToDec('100') -> 4
  * 	binToDec('101') -> 5
@@ -14,7 +14,24 @@
  */
 
 function binToDec(binary) {
-
+  // binary starts at zero
+  // declare binary + '' to turn into String
+  // assign length - 1 to var exponent
+  let decimal = 0;
+  let binStr = binary + "";
+  while (binStr) {
+    if (binary % 2 !== 0) {
+      decimal += 1;
+      binStr = Number(binStr) - 1 + ""; //?
+    }
+    binStr = Number(binStr) + ""; //?
+    let power = binStr.length - 1; //?
+    decimal = decimal += Math.pow(2, power);
+    binStr.slice(1); //?
+  }
+  return decimal;
 }
+
+binToDec(101); //?
 
 module.exports = binToDec;
