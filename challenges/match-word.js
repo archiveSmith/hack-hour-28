014a5bf74a7970059f042557dfdc4849b03a31e7
Johnny Lim <bluejayl@ucla.edu>
2019-04-16 09:55:33 -0700

    match word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -12,6 +12,42 @@
 
 function matchWord(str) {
 
+  if (str.length === 0) {
+    return true
+  };
+
+  let stringArr = str.match(/[a-zA-Z]+/g);
+  let stack = [];
+
+  console.log(stringArr);
+  
+  for (let i = 0; i < stringArr.length; i++) {
+    let currentStr = stringArr[i].toLowerCase().split('').reverse().join('');
+    console.log(currentStr);
+
+    if(currentStr = stack[stack.length - 1]) {
+      stack.pop()
+   
+    } else {
+      stack.push(stringArr[i].toLowerCase());
+      
+    }
+    
+  }
+
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(matchWord('__END_DNE-----'))
+console.log(matchWord('__ENDDNE__'));
+console.log(matchWord('IF()()fi[]'));
+console.log(matchWord('for__if__rof__fi'))
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'))
+console.log(matchWord(''));
+
 
 module.exports = matchWord;
