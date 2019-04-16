4a2d1cd2a464ab470e69050539725dc6fa61cfe3
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-04-16 09:55:25 -0700

    attempt match word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,17 +1,27 @@
 // Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", 
-//i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that 
-//takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or 
-//punctuation.
+    //i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that 
+    //takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or 
+    //punctuation.
 
-// matchWord('__END_DNE-----');  -> true
-// matchWord('__ENDDNE__');  -> false       (not separated by a space)
-// matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
-// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
-// matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
-// matchWord('');  -> true
+    // matchWord('__END_DNE-----');  -> true
+    // matchWord('__ENDDNE__');  -> false       (not separated by a space)
+    // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
+    // matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
+    // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
+    // matchWord('');  -> true
 
-function matchWord(str) {
-
-}
+    const matchWord = (str) => {
+        if(str === ""){
+          return true;
+        } 
+        let regex = /[^\w\s]/g;
+        let found = str[str.search(regex)];
+      
+        if(found !== undefined){
+          return true;
+        } 
+        return false;
+        
+      }
 
 module.exports = matchWord;
