e4b9a67464f8bba70fd602b3b77d9a4acbf0125e
Keith <keith.avila@gmail.com>
2019-04-17 09:44:28 -0700

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,24 @@
  */
 
 function highestProduct(array) {
-
+  let product = 1;
+  if (array.length === 0) return 0;
+  else if (array.length < 4) {
+    for (let i = 0; i < array.length; i++) {
+      product *= array[i];
+    }
+  } else {
+    function sortNumber(a,b) {
+      return b - a;
+    }
+    let newArr = array.sort(sortNumber);
+    for (let i = 0; i < 3; i ++) {
+      product *= +newArr[i];
+    }
+  }
+  return product;
 }
 
+console.log(highestProduct([1,2,-10,4,-5,6]))
 
 module.exports = highestProduct;
