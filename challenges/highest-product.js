3b41ccf9f79ed20c6bc822cb47e6b903201d8ac7
CBABY! <cbaby@CBABYs-MacBook-Pro.local>
2019-04-17 09:38:43 -0700

    finished w/ recursive solution

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,9 +2,18 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
-
+function highestProduct(array, maxArr = array.sort((a, b) => b - a).slice()) {
+  if (maxArr.length === 3) return productOfArray(maxArr);
+  maxArr.pop();
+  return highestProduct(array, maxArr);
 }
 
+function productOfArray(arr) {
+  return arr.reduce((acc, el) => {
+    return acc * el;
+  })
+}
+
+highestProduct([ 1, 1, 2, 5, 2, 3, 4 ])//?
 
 module.exports = highestProduct;
