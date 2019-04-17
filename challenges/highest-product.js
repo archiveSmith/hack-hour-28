fb088fcf6d433d66c641f43cb5eb86e703837f34
Aidan Noble-Goodman <anoblegoodman@Aidans-MacBook-Pro.local>
2019-04-17 09:38:10 -0700

    highest-product completed

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,18 @@
  */
 
 function highestProduct(array) {
-
+  array.sort(function(a, b){return a - b})
+  let newArr = [];
+  for(let i = array.length - 1; i >= array.length - 3; i -= 1){
+    newArr.push(array[i])
+  }
+  const add = (a, b) => a + b;
+  const sum = newArr.reduce(add)
+  return sum;
 }
 
 
+
 module.exports = highestProduct;
+console.log(highestProduct([4, 3, 7, 1, 90]))
+console.log(highestProduct([25, 1, 3, 6, 2, 2, 5]))
