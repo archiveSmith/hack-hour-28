977d7ba97a7247ea6dbd73e91804298673000faf
Sam Selfridge <Sam.selfridge@gmail.com>
2019-04-17 09:30:29 -0700

    positive integers.  No length checking or negative number handling

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,25 @@
  */
 
 function highestProduct(array) {
+    let sorted = array.sort(function compareNumbers(a, b) {
+        return a - b;
+      });
+    let product;
+    sorted
+    array
+    
+    let endex = array.length - 1;
+    let first = array[endex]
+    let second = array[endex - 1]
+    let third = array[endex - 2]
 
+    product = first * second * third;
+    return product;
 }
 
+
+let asdf = highestProduct([ 4, 5, 6, 7, 8, 23]);
+
+asdf
 
 module.exports = highestProduct;
