5056d890ffe0ad5f35f33b4f3098a66a149168c3
Steven <stevj17@gmail.com>
2019-04-17 09:35:05 -0700

    hr

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,27 @@
  */
 
 function highestProduct(array) {
-
+	let result = 1;
+	let sorted = array.sort(sortNumber); // Sorts array
+	let sliced = sorted.slice(-3); // Slices for the highest last three numbers
+	sliced.map(el => {  // Maps, result * el
+		result *= el;
+	});
+	return result;
+	
 }
 
+// Create a function to sort numbers
+function sortNumber(a,b) {
+	return a - b;
+}
+
+const numArray = [1, 6, 3, 5, 12, 8];
+numArray.sort(sortNumber);
+//console.log(numArray.join(","));
+
+
+
+console.log(highestProduct(numArray));
 
 module.exports = highestProduct;
