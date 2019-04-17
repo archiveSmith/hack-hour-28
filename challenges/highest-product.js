1c8c4277a7f92317fbe135d60b71609484770ade
j218 <jaymlim93@gmail.com>
2019-04-17 09:30:46 -0700

    done with highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
-
+    let sortedArr = array.sort((a,b) => b - a);
+    console.log(sortedArr);
+    let highestVals = sortedArr.slice(0,3);
+    return highestVals.reduce((acc, curr) => acc * curr);
 }
+// console.log(highestProduct([5,3,1,7,9,2]));
+// console.log(highestProduct([-3,-9,-6,-1, -2, 0]));
+// console.log(highestProduct([-3,-9,-6,-1, -2]));
+// console.log(highestProduct([-3,-9,-6,-1, -2, 1, 9]));
 
 
 module.exports = highestProduct;
