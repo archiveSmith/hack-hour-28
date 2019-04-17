300434f7a63fd948e388a3388df87f8287df25f5
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-04-17 09:36:24 -0700

    completed highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,9 +2,42 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
 
+function highestProduct(array) {
+    let negNums = [];
+    let finalNums = [];
+    let finalProduct = 1;
+
+    array.forEach(num => {
+        if (num < 0) negNums.push(num);
+        if (num > 0) finalNums.push(num);
+    })
+
+    if (negNums.length % 2 === 0) {
+        negNums.forEach(negNum => {
+            finalNums.push(negNum)
+        })
+    } else {
+        let highestNegNum = -Infinity;
+        negNums.forEach(negNum => {
+            if (negNum > highestNegNum) highestNegNum = negNum;
+        })
+        negNums.forEach(negNum => {
+            if (negNum != highestNegNum) finalNums.push(negNum);
+        })
+    }
+
+    for (let i = 0; i < finalNums.length; i += 1) {
+        finalProduct = finalProduct * finalNums[i];
+    }
+
+    return finalProduct;
 }
 
+
+// console.log(highestProduct([0,1]));
+// console.log(highestProduct([1,2,5,10]))
+// console.log(highestProduct([1,-1,2,-2,-3]))
+// console.log(highestProduct([-2,-3,4,5, -1]))
 
 module.exports = highestProduct;
--- challenges/match-word.js
+++ challenges/match-word.js
@@ -12,6 +12,52 @@
 
 function matchWord(str) {
 
+    const alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    const wordList = []; 
+    let tracker = '';
+    let match = false;
+    if (str === '') return true;
+    for (let i = 0; i < str.length; i += 1) {
+
+        if (alph.includes(str[i].toLowerCase())) {
+            tracker += str[i].toLowerCase();
+        
+        } else if (tracker != '' && !alph.includes(str[i])) {
+            wordList.push(tracker);
+            tracker = '';
+        } else if (i === str.length - 1 && tracker != '') {
+            wordList.push(tracker);
+        }
+    }
+    
+    console.log(wordList)
+    if (wordList.length === 1 && wordList[0] != '') return false;
+
+    wordList.forEach(word => {
+        // let match = word.split('').reverse().join('');
+        console.log(word)
+        for (let i = wordList.length - 1; i >= 0; i -= 1) {
+            console.log(wordList[i])
+            console.log(word)
+            console.log(wordList[i].split('').reverse().join(''))
+            if (wordList[i].split('').reverse().join('') === word) {
+                match = true;
+            } else if (wordList[i].split('').reverse().join('') != word) {
+                match = false;
+            }
+        }
+    })
+    return match;
 }
+
+
+// console.log(matchWord('__END_DNE-----'));
+// console.log(matchWord('__ENDDNE__'));
+// console.log(matchWord('IF()()fi[]'));
+console.log(matchWord('for__if__rof__fi'));
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
+// console.log(matchWord(''));
+
+
 
 module.exports = matchWord;
