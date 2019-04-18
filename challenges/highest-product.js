c072ff35eeea65b537468dae8e0cb936af205f91
Johnny Lim <bluejayl@ucla.edu>
2019-04-18 08:59:38 -0700

    completed highest-produc

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -1,10 +1,29 @@
 /**
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
+let n = [ -4, 2, -8, 1];
+
+
 
 function highestProduct(array) {
+  
+  const nums = [];
 
+  array.forEach( (el)=> {
+    nums.push(Math.abs(el));
+  })
+
+  let sortedArr = nums.sort( (a,b) => {
+    return a - b 
+  }) 
+
+
+  const length = nums.length;
+  const highestProduct = sortedArr[length - 1] * sortedArr [length - 2] * sortedArr[length - 3]
+
+  return highestProduct;
 }
+
 
 
 module.exports = highestProduct;
