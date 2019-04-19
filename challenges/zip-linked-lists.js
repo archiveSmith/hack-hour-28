721a5e21d2a39b0249c2cb170ebb0927bb6e0dc9
Steven <stevj17@gmail.com>
2019-04-19 09:59:13 -0700

    submit

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,29 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
-};
+function zip(l1, l2) {  
+  // See if either lists are empty
+  if(l1 == null) return l2; 
+  if(l2 == null) return l1;
+
+  let recur = new Node(zip(l1.next, l2.next)); // Recursive call first 
+  let result = new Node(l1); 
+  l1.next = l2;  // one node from A
+  l2.next = recur; 
+
+  return result;
+
+}; 
+
+let newNode1 = new Node('item1');
+let newNode2 = new Node('item2');
+let newNode3 = new Node('item3');
+let newNode4 = new Node('item4');
+let linkedList1 = {newNode1, newNode2};
+let linkedList2 = {newNode3, newNode4};
+
+let zipped = zip(linkedList1, linkedList2);
+
+
 
 module.exports = {Node: Node, zip: zip};
