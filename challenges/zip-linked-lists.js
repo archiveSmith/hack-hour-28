8a44502c4ee14643b9168ad16dbdd826cfbfc10c
Jonathan Peralez <jonathanperalez@Jonathans-MacBook-Pro.local>
2019-04-20 09:15:52 -0700

    Did not finish

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,32 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  // set odd counter
+  let isOdd = true;
+
+  let l1Next = l1.next;
+  let l2Next = l2.next;
+  while (l1Next) {
+    while (l2Next) {
+      if (l1.value != null) {
+        const newL1Node = new Node(l1.value);
+      }
+      if (l2.value != null) {
+        const newL2Node = new Node(l2.value);
+      }
+      if (isOdd) {
+        newL1Node.next = newL2Node;
+      } else {
+        newL2Node.next = newL1Node;
+      }
+
+      // reverse value of isOdd
+      isOdd = !isOdd;
+    }
+  }
 };
 
-module.exports = {Node: Node, zip: zip};
+module.exports = {
+  Node: Node,
+  zip: zip
+};
