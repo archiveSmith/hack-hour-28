bac3266d4a840c9b3c81bba1d46d28bd2ef00767
alyviam <alyvialmoss@gmail.com>
2019-04-20 10:02:03 -0700

    Alyvia Max-Stack Hack Hour

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,45 @@
  */
 
 function Stack() {
-  // body...
+  this.stack = [[]];
+  this.length = 0;
+  this.maxValue = 0;
+
+
+  Stack.prototype.push = val => {
+    this.stack[0][this.length] = val;
+    this.length += 1;
+    if (this.maxValue < val) {
+      this.maxValue = val
+    };
+    return this.length;
+  }
+
+  Stack.prototype.pop = () => {
+    const element = this.stack[0][this.length - 1];
+    delete this.stack[0][this.length - 1];
+    this.length -= 1;
+    return element;
+  }
+
+  Stack.prototype.maxVal = () => {
+    if (!this.stack[0].includes(this.maxValue)) {
+      this.maxValue = this.stack[0].reduce((acc, current) => {
+        return acc = Math.max(acc, current);
+      }, 0)
+    }
+    return this.maxValue;
+  }
+
 }
+
+
+const test = new Stack();
+console.log(test.push(5));
+console.log(test.push(7));
+console.log(test.push(6));
+console.log(test.push(9));
+console.log(test.pop());
+console.log(test.maxVal())
 
 module.exports = Stack;
