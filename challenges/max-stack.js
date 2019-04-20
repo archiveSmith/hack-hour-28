a3741f1ba276b014555a4bb1cd0d33a156e32856
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-04-20 09:26:51 -0700

    completed max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,34 @@
  */
 
 function Stack() {
-  // body...
+  // create a stack
+  this.stack = [];
+  this.length = 0;
+}
+
+// push: return new length
+Stack.prototype.push = (val) => {
+  this.stack[this.length] = val;
+  return ++this.length;
+}
+
+// pop: return element just removed
+Stack.prototype.pop = () => {
+  if (this.length === 0) return undefined;
+  let temp = this.stack[--this.length];
+  delete this.stack[this.length];
+  return temp;
+}
+
+// getMax: return largest value currently in stack
+  // bonus: retrieve max calue from stack in 0(1) time
+  Stack.prototype.getMax = () => {
+    if (this.length === 0) return undefined;
+    let max = null;
+    this.stack.forEach((num) => {
+      if (max === null || max < num) max = num;
+    });
+    return max;
 }
 
 module.exports = Stack;
--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -5,12 +5,58 @@
  * BONUS: Do this in place
  */
 
+// function LinkedList() {
+//   this.head = null;
+//   this.tail = null;
+// }
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
 function zip(l1, l2) {
+  //declare a count called l1NodesUsed for l1 and l2NodesUsed for l2
+  let l1NodesUsed = 0;
+  let l2NodesUsed = 0;
+  //if l1's length is 0, return l2
+  if (l1.length === 0) return l2;
+  //l2's length is 0, return l1
+  if (l2.length === 0) return l1;
+  // set starting values
+  let l2curr = l2.head;
+  let l1curr = l1.head;
+  // determine which list will run out first
+  let shorter;
+  let shorterUsed;
+  if (l1.length < l2.length) {
+    shorter = l1.length;
+    shorterUsed = l1NodesUsed
+  } else {
+    shorter = l2.length;
+    shorterUsed = l2NodesUsed;
+  }
+  //otherwise, set l1s head as the head of the zip list.
+  let l1.head.next = l2curr;
+  l1.head.next = l2.head;
+  while (shorterUsed <= shorter) {
+    curr = l2curr;
+    curr.next = l1curr.next;
+    l1NodesUsed += 1;
+    l1curr = l1curr.next;
+    l2NodesUsed += 1;
+    l2curr = l2curr.next;
+  }
+  //compare count to l2's length. If l2's length is exhausted, slice the remaining nodes off of it and add to zip list.
+
+  //iterate l1NodesUsed count
+  //set l2's head to the next node in the zip list
+  //compare count to l1's length. If l1's length is exhausted, slice the remaining nodes off of it and add to zip list. 
+  //repeat.
+  //return when both are exahusted. 
+
 };
+
+console.log(zip(,));
 
 module.exports = {Node: Node, zip: zip};
