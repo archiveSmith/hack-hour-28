9a942ef118acbf812638853828437c382688378d
Karen Pinilla <pinillakaren11@gmail.com>
2019-04-20 09:44:05 -0700

    max stack finished

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,39 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {}
+  this.index = 0;
 }
 
+Stack.prototype.push = function (value) {
+  this.storage[this.index] = value;
+  this.index +=1
+}
+
+Stack.prototype.pop = function () {
+  const lastElement = this.storage[this.index - 1];
+  delete this.storage[this.index - 1];
+  this.index -= 1
+  return lastElement;
+}
+
+Stack.prototype.getMax = function() {
+  console.log(this.index)
+  let max = 0;
+  for (let i = 0; i < this.index; i++) {
+    if (this.storage[i] > max)
+      max = this.storage[i]
+  }
+  return max;
+}
+
+let stack = new Stack;
+stack.push(5)
+stack.push(8)
+stack.push(4)
+// stack.pop()
+console.log(stack.getMax())
+
+
+console.log(stack)
 module.exports = Stack;
