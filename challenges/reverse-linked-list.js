4aa8f38be3376ecb7cceffceb7f4476d00ffbead
j218 <jaymlim93@gmail.com>
2019-04-23 09:57:23 -0700

    reverse-ll

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,35 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    let node = head;
+    let reversal;
+    let temp;
+    while(node) {
+        temp = node.next;
+        node.next = reversal;
+        reversal = node;
+        node = temp;
+    }
+    return reversal;
 }
+
+function makeList(array) {
+    let head = new Node(array[0]);
+    let curr = head;
+    array.forEach((val, index) => {
+      if (index === 0) return; //skip first element
+      const newNode = new Node(val);
+      curr.next = newNode;
+      curr = newNode;
+    });
+    // console.log(head.value);
+    return head;
+  }
+
+  let list = makeList([1,2,3]);
+  console.log(list)
+  console.log(reverseLinkedList(list));
+  
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
