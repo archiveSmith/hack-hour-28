37c515346214826735ba881c0102424a8aad0ffc
RadicalRoy <rolandquintana1991@gmail.com>
2019-04-23 09:41:44 -0700

    Reversing from bed baby!

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -15,6 +15,34 @@ function Node(value) {
 
 function reverseLinkedList(head) {
 
+  let cur = head.next;
+  let temp = head.next;
+  let reversed = head;
+  head.next = null;
+
+  while(temp) {
+    temp = temp.next;
+    cur.next = reversed;
+    reversed = cur;
+    cur = temp;
+  }
+
+  return reversed;
+
 }
+
+let a  = new Node(1)
+let b  = new Node(2)
+let c  = new Node(3)
+let d  = new Node(4)
+
+a.next = b
+b.next = c
+c.next = d
+
+
+console.log(reverseLinkedList(a));
+
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
