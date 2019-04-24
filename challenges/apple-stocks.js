a5182818bc0a0f5a7ddb672ff4022e00b581d6d8
Sam Selfridge <Sam.selfridge@gmail.com>
2019-04-24 09:47:16 -0700

    Travis tell me how this doesn't work while I write test cases

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -12,8 +12,68 @@
  *  Return 0 if no profit is possible OR if input is invalid.
  */
 
-function bestProfit(stock_prices_yesterday) {
+function bestProfit(array) {
+    if (Array.isArray(array) == false) return 0;
 
+    //handle best case where the highest value of the day is after the lowest
+    let totalMax = Math.max.apply(Math, array)
+    let totalMin = Math.min.apply(Math, array)
+    if (array.indexOf(totalMin) < array.indexOf(totalMax)) {
+        return totalMax - totalMin;
+    }
+
+    let lows = [];
+    let highs = [];
+
+    let prev = array[0];
+    let curr = array[0];
+    for (let i = 0; i < array.length; i++) {
+        const next = array[i];
+        if (next > curr && curr < prev) {
+            lows.push(i-1);
+        } else if (next < curr && curr > prev) {
+            highs.push(i-1);
+        }
+        prev = curr;
+        curr = next;    //skips last index
+    }
+
+    //if last price of the day is higher than its previos price ad it to the list of highs to check
+    if(array[length -1] > array[length -2]){
+        highs.push(length -1);
+    }
+
+
+
+    let buyTime,sellTime;
+    let maxProfit = 0;
+    for (let i = 0; i < lows.length; i++) {
+        const lowPrice = array[lows[i]];
+
+        for (let j = 0; j < highs.length; j++) {
+            if(lows[i] > highs[j]) continue; // current low index is higher than this high, skip comparison
+            const highPrice = array[highs[j]];
+            let currProfit = highPrice - lowPrice;
+            if(currProfit > maxProfit) {
+                buyTime = i;
+                sellTime = j;
+                maxProfit = currProfit
+            }
+        }
+    }
+
+    
+
+    return maxProfit
 }
+
+
+
+let prices = [5, 6, 7, 6, 4, 5, 8, 12, 12, 5, 4, 10]
+
+let moneys = bestProfit(prices)
+
+console.log(moneys);
+
 
 module.exports = bestProfit;
