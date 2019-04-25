34a7da741e18260513d409de160adc676d7a4377
julicious100 <beakjulie@gmail.com>
2019-04-25 09:26:22 -0700

    hackhour apr/25

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,14 +7,21 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time  
+ *
+ * BONUS:
+ * Complete in O(n) time
  *
  */
+// input: arrays
+// output: array
 
 function mergeArrays(arr1, arr2) {
-
+  let mergedArr = arr1.concat(arr2);
+  // console.log(mergedArr);
+  mergedArr.sort((a, b) => a - b);
+  // console.log(mergedArr);
+  return mergedArr;
 }
 
+mergeArrays(my_array, another_array);
 module.exports = mergeArrays;
