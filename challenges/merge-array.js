a694d18c768ae2da47337cac5402ecb4d574cd4a
Timothy Johnson <Timmy@Timothys-MacBook-Pro.local>
2019-04-25 09:21:39 -0700

    Done

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,14 +7,18 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time  
+ *
+ * BONUS:
+ * Complete in O(n) time
  *
  */
 
 function mergeArrays(arr1, arr2) {
-
+  let merge = arr1.concat(arr2);
+  let sorted = merge.sort((a,b) => a - b);
+  console.log(sorted);
 }
+
+//console.log(mergeArrays([1,2,3],[3,2,1]));
 
 module.exports = mergeArrays;
