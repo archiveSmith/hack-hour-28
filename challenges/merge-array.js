b3aec1530de4c2a6b62270ed401320e03601d880
j218 <jaymlim93@gmail.com>
2019-04-25 09:21:28 -0700

    done with merge array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,14 @@
  */
 
 function mergeArrays(arr1, arr2) {
-
+    const merged = arr1.concat(arr2);
+    console.log(merged);
+    merged.sort((a,b) => a - b);
+    console.log(merged);
+    return merged;
 }
+var my_array = [3,4,6,10,11,15,21];
+var another_array = [1,5,8,12,14,19];
+// console.log(mergeArrays(my_array, another_array));
 
 module.exports = mergeArrays;
