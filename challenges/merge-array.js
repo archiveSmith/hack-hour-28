806c0844b0e8757e11eba5be79c37516c01dae05
Keller <keller@Kellers-MacBook-Air.local>
2019-04-25 09:42:33 -0700

    merge-array done

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -1,6 +1,6 @@
 /**
  * We have our lists of orders sorted numerically already, in arrays.
- * Write a functin to merge our arrays of orders into one sorted array.
+ * Write arr1 functin to merge our arrays of orders into one sorted array.
  * These may be of different lengths.
  *
  * var my_array = [3,4,6,10,11,15,21];
@@ -14,7 +14,29 @@
  */
 
 function mergeArrays(arr1, arr2) {
+  const mergedArr = [];
+  const arr1Copy = [...arr1];
+  const arr2Copy = [...arr2];
 
+  while (arr1Copy.length || arr2Copy.length) {
+    if (typeof arr1Copy[0] === 'undefined') {
+      mergedArr.push(arr2Copy[0]);
+      arr2Copy.splice(0, 1);
+    } else if (arr1Copy[0] > arr2Copy[0]) {
+      mergedArr.push(arr2Copy[0]);
+      arr2Copy.splice(0, 1);
+    } else {
+      mergedArr.push(arr1Copy[0]);
+      arr1Copy.splice(0, 1);
+    }
+  }
+
+  return mergedArr;
 }
 
-module.exports = mergeArrays;
+var my_array = [3, 4, 6, 10, 11, 15, 21];
+var another_array = [1, 5, 8, 12, 14, 19];
+
+console.log(mergeArrays(my_array, another_array));
+
+// module.exports = mergeArrays;
