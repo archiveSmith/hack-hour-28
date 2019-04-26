2f2c06bc3cd2c7d9760d22f56fc64c3d66a7ced3
mapleseventh <mapleseventh@gmail.com>
2019-04-26 09:52:54 -0700

    num to wrods draft

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -11,9 +11,47 @@
  * numToWords(300525151340440) -> 'ThreeHundredTrillionFiveHundredTwentyFiveBillionOneHundredFiftyOneMillionThreeHundredFortyThousandFourHundredForty'
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
-
-function numToWords(num) {
-
+const hash = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fiveteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+    100: 'hundred',
+    1000: 'thousand',
+    1000000: 'million',
+    1000000000: 'billion',
+    1000000000000: 'trillion',
 }
 
+
+function numToWords(num) {
+    const numStr = new String(num);
+    let length = numStr.length;
+
+    
+}
+console.log(str = new String(100).length)
 module.exports = numToWords;
