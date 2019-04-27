6b3b90107a03d16c02b171197687f0d5b258b26d
julicious100 <beakjulie@gmail.com>
2019-04-27 09:34:38 -0700

    hackhour apr/27

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,20 @@
  */
 
 function permPalin(str) {
-	
-}
+  const HASH = {};
+  let count = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (!HASH[str[i]]) HASH[str[i]] = 1;
+    else HASH[str[i]] += 1;
+  }
+
+  for (k in HASH) {
+    if (HASH[k] % 2 !== 0) count += 1;
+    if (count > 1) return false;
+  }
+
+  return true;
+} // => true
 
 module.exports = permPalin;
