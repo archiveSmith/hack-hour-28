793b7497d5ef65c8d24928c051a2b9ee791a7f9a
Sam Selfridge <Sam.selfridge@gmail.com>
2019-04-27 09:27:33 -0700

    Counting is fun

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,57 @@
  */
 
 function permPalin(str) {
-	
+    let countObj = {}
+    let strArray = str.split('');
+
+    let strEven = str.length % 2 === 0;
+
+    strEven
+
+    strArray.forEach(letter => {
+        if(countObj[letter]){
+            countObj[letter]++;
+        }else{
+            countObj[letter] = 1;
+        }
+    });
+
+    countObj;
+
+    let oddArray = [];
+
+    for (const letter in countObj) {
+        if (countObj.hasOwnProperty(letter)) {
+            const count = countObj[letter];
+            if(count % 2 === 1) oddArray.push(letter);
+        }
+    }
+
+    //even length str with odd letter count
+    if(strEven && oddArray.length != 0) return false; 
+
+    //even length str with no odd letter counts
+    if(strEven && oddArray.length === 0) return true; 
+
+    //Odd string length with only 1 odd character
+    if(!strEven && oddArray.length === 1) return true;
+
+    //Odd string length and more than 1 odd chacters
+    if(!strEven && oddArray.length !== 1) return false;
+
+    console.log("Default - we shouldn't get here");
+    return false
 }
+
+
+// out = permPalin('cbaba')// => true
+// out
+// out = permPalin('cbac') //=> false
+// out
+
+// out = permPalin('bbaa') 
+// out
+// out = permPalin('a')
+// out
 
 module.exports = permPalin;
