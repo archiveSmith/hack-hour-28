c0e5666986cf8046c868412303bd4fc548cb94c4
Steven <stevj17@gmail.com>
2019-04-27 09:16:56 -0700

    hackhour4.27

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,6 +10,20 @@
  */
 
 function permPalin(str) {
+    const split = word
+    .replace(/\s/g,'')
+    .toLowerCase()
+    .split('');
+  
+  if (split.length === 1) return true;
+
+  const repeat = split.reduce((agg, letter) => ({
+    ...agg,
+    [letter]: agg[letter] ? agg[letter] + 1 : 1,
+  }), {});
+
+  return Object.values(repeat)
+    .filter(val => val % 2).length <= 1;
 	
 }
 
