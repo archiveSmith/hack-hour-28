191e52b4860dc8d175c585167072ad7b8e7f9e95
Keller <keller@Kellers-MacBook-Air.local>
2019-04-27 09:36:36 -0700

    check for perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,30 @@
  */
 
 function permPalin(str) {
-	
+  if (str.length === 0) return false;
+  if (str.length === 1) return true;
+
+  const strArr = str.split('');
+  const letters = {}
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (!letters[strArr[i]]) {
+      letters[strArr[i]] = 0;
+    }
+    letters[strArr[i]] += 1;
+  }
+
+  let oneOdd = false;
+
+  for (let prop in letters) {
+    if (oneOdd && letters[prop] === 1) {
+      return false;
+    } else if (letters[prop] % 2 === 1) {
+      oneOdd = true;
+    }
+  }
+
+  return true;
 }
 
 module.exports = permPalin;
