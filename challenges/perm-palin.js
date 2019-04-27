760c130adac048ec51610a54937953a26c15b3e1
Sam Ryoo <samryoo@gmail.com>
2019-04-27 09:38:01 -0700

    finished

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -8,9 +8,25 @@
  * 	- permPalin('a') => true
  *
  */
-
 function permPalin(str) {
-	
+    let letterCount = {};
+    let countOdds=0;
+    for (let i =0; i<str.length; i++) {
+        if (letterCount[str[i]] === undefined) {
+          letterCount[str[i]] = 1;
+        } else {
+        letterCount[str[i]] += 1;
+        }
+    }
+    let values = Object.values(letterCount);
+    for (let j=0; j<values.length; j++) {
+      if (values[j]%2===1) {
+        countOdds++;
+      }
+    }
+    if (countOdds===1) return true;
+    else if (countOdds%2===1) return false
+    return true;
 }
 
 module.exports = permPalin;
