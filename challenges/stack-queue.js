81ac070c00b413d335028cd2de03ee72791f8614
steven <stevj17@gmail.com>
2019-04-30 10:00:36 -0700

    submission

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -2,19 +2,27 @@
  * Create a stack.Then create a queue using two stacks.
  */
 
+ const Stack1 = [];
+ const Stack2 = [];
 
-function Stack() {
+ function Stack(element) {
+   Stack1.push(element);
+ }
 
-}
+ function Queue() {
+   if (Stack2.length === 0) {
+     if (Stack1.length === 0) { return 'Queue is empty...'; }
+     while (Stack1.length > 0) {
+       let p = Stack1.pop();
+       Stack2.push(p);
+     }
+   }
+   return Stack2.pop();
+ }
 
-
-/**
-* Queue Class
-*/
-
-
-function Queue() {
-
-}
+ Stack('a');
+ Stack('b');
+ Stack('c');
+ Queue();
 
 module.exports = {Stack: Stack, Queue: Queue};
