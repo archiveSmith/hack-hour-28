23808a1186de28c964983ba8bbe7701922fabac4
alyviam <alyvialmoss@gmail.com>
2019-04-30 09:52:30 -0700

    push of hack-hour stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,17 +4,29 @@
 
 
 function Stack() {
-
+	return [...arguments]
 }
-
-
-/**
-* Queue Class
-*/
-
 
 function Queue() {
+	const stackOne = Stack();
+	const stackTwo = Stack();
 
+	function enqueue(el) {
+		stackOne.push(el);
+	}
+	function dequeue() {
+		if (stackTwo.length === 0) {
+			if (stackOne.length === 0) {
+				return 'Queue is empty';
+			}
+			while (stackOne.length > 0) {
+				let popEl = stackOne.pop();
+				stackTwo.push(popEl);
+			}
+			return stackTwo.pop();
+		}
+	}
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+console.log(Stack(5))
+module.exports = { Stack: Stack, Queue: Queue };
