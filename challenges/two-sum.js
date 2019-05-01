29c191ca59162a48ae464e759657da5e0c0be1b8
j218 <jaymlim93@gmail.com>
2019-05-01 09:22:33 -0700

    done with twosum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,20 @@
  */
 
 function twoSum(arr, n) {
-
+    if(!Array.isArray(arr)) return undefined;
+    let sum = 0; 
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] === n - arr[i]) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
+
+const sumArr = [1,2,2,5,6,1]
+const target = 5;
+console.log(twoSum(sumArr, target))
 
 module.exports = twoSum;
