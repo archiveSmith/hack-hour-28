6b1b224d9e059731bd286366d21ff114854595f2
Sam Selfridge <Sam.selfridge@gmail.com>
2019-05-01 09:28:02 -0700

    two-sum seems like a easier version of another one we did....

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,43 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
-
+//return true if n is in array
+function checkSub(arr, n) {
+    // console.log(`Check ${arr} for ${n}`);
+    let index = arr.indexOf(n)
+    return (index != -1)
 }
 
+
+function twoSum(arr, n) {
+
+    for (let index = 0; index < arr.length; index++) {
+        const num = arr[index];
+        
+    
+
+    
+        let subArray1 = arr.slice(0, index)
+        let subArray2 = arr.slice(index + 1)
+
+        // subArray1
+        // subArray2
+
+        const subArray = subArray1.concat(subArray2)
+        // subArray
+        if (checkSub(subArray, n - num)) {
+            // console.log(`Found ${n-num} True for ${n} and ${num}`);
+            return true;
+        }
+    };
+
+
+    return false;
+}
+
+
+// const arr = [1, 3, 4, 5, 6, 8]
+
+// let out = twoSum(arr, 145)
+// out
 module.exports = twoSum;
