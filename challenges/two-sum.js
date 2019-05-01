80803c6543e269ef38b7ce92ec2f721b8aaa7899
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-05-01 09:28:17 -0700

    attempt two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,7 +2,21 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
+const twoSum = (arr, n) => {
+    //store differences that we are searching for
+    let numNeeded = {};
+
+    //iterate through and subtrack from n
+    for(let i = 0; i < arr.length; i++) {
+        let searchVal = Math.abs(n-arr[i]);
+        if(numNeeded[searchVal] === undefined){
+            numNeeded[searchVal] = searchVal;
+        }
+        if(arr[i-1] === numNeeded[searchVal]){
+            return true;
+        }
+    }
+    return false
 
 }
 
