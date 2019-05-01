d4717b79a9901442a067e3d8f4067233a45b91d1
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2019-05-01 09:47:17 -0700

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,21 @@
  *  array that sum up to the target value; return false otherwise
  */
 
+
+console.log(twoSum([1,2,3,4,5], ))
 function twoSum(arr, n) {
-
+const sortArr = arr.sort((a, b) => a - b);
+let switchy = false;
+  for (let i = 0; i < arr.length; i++) {  
+    for (let j = i + 1; j < arr.length; j++) {
+      if (n > sortArr[i]) {
+        let target = n - sortArr[i];
+          if (sortArr[j] === target) {
+            switchy = true
+          }
+      } 
+    }
+  }
+  return switchy
 }
-
 module.exports = twoSum;
