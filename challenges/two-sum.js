e383460b998a729ab5a293cce0a7a3ac995584b2
steven <stevj17@gmail.com>
2019-05-01 09:33:49 -0700

    submission

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,31 @@
-/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+/*  Given an array of numbers and a target number, return true if there are two numbers in the
  *  array that sum up to the target value; return false otherwise
  */
+const targetNum = 13;
+const sampleArr = [4, 6, 2, 7, 3];
 
 function twoSum(arr, n) {
 
+  const map = {},
+          pair, pairMaxIndex = arr.length - 1;
+
+      for (const i = 0; i <= pairMaxIndex; i++) {
+          let a = arr[i];
+          let b = n - a;
+          let j = map[b];
+          if (j !== undefined && i <= pairMaxIndex && j <= pairMaxIndex) {
+              pairMaxIndex = i > j ? i : j;
+              pair = i < j ? [a, b] : [b, a];
+          }
+          let tmp = map[a];
+          if (tmp === undefined || i < tmp) {
+              map[a] = i;
+          }
+      }
+      return pair[0] + pair[1] === n ? true : false;
 }
+
+console.log(twoSum(sampleArr, targetNum));
+
 
 module.exports = twoSum;
