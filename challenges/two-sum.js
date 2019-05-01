6eb7f62d02b2bbb14f496ed32a55531126de0710
Keller <keller@Kellers-MacBook-Air.local>
2019-05-01 09:43:18 -0700

    two-sum brute force/greedy approach

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,6 +3,37 @@
  */
 
 function twoSum(arr, n) {
+  // const obj = {};
+
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   obj[i] = arr[i];
+  // }
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   const target = n - arr[i];
+  //   for (let j = i + 1; j < arr.length; j++) {
+  //     if (arr[j] === n - arr[i]) {
+  //       return console.log(true)
+  //     };
+  //   }
+  // }
+
+  // return false;
+
+  const map = {};
+  const results = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] !== undefined) {
+      results.push([map[arr[i]], arr[i]])
+    } else {
+      map[n - arr[i]] = arr[i];
+    }
+  }
+
+  if (results.length > 0) return true;
+  return false;
 
 }
 
