8f5e21ec2c40289adca74a311641521094c04a7e
Keith <keith.avila@gmail.com>
2019-05-01 09:44:29 -0700

    two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,22 @@
  */
 
 function twoSum(arr, n) {
-
+  let pairObj = {};
+  let result = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (pairObj[arr[i]]) {
+      result = true;
+      break;
+    }
+    else pairObj[n - arr[i]] = true;
+  }
+  return result;
 }
 
+
+let arr = [1, 9, 7]
+// let arr = [2, 4, 7]
+// let arr = [2, 4, 7, 5, 6]
+
+console.log(twoSum(arr, 10));
 module.exports = twoSum;
