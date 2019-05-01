d0007349c02372b50b9f0348b372c593794cbda7
RadicalRoy <rolandquintana1991@gmail.com>
2019-05-01 09:19:12 -0700

    Two step two step two step

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,26 @@
-/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+/*  Given an array of numbers and a target number, return true if there are two numbers in the
  *  array that sum up to the target value; return false otherwise
  */
 
 function twoSum(arr, n) {
 
+  if(!arr || !n) return false;
+
+  let terms = new Set;
+
+  for(let i = 0; i < arr.length; i++){
+    if(terms.has(n - arr[i])) return true;
+
+    terms.add(arr[i]);
+  }
+
+    return false;
 }
+
+/*
+const arr = [3, 4, 5, 6, 7];
+
+console.log(twoSum(arr));
+*/
 
 module.exports = twoSum;
