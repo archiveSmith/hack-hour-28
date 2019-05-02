eaa468e3145cb1b94265f66f43d45dab2a36b7ef
Sam Selfridge <Sam.selfridge@gmail.com>
2019-05-02 09:23:51 -0700

    Coffee is calling

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,7 +4,7 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
+
 
 function BinaryTree(val) {
     this.value = val;
@@ -12,8 +12,20 @@ function BinaryTree(val) {
     this.right = null;
 }
 
-function validBST(tree) {
+function validBST(tree, shouldBeLessThan = null, shouldBeGreaterThan = null) {
+    if (tree === null) return true;
 
+
+    if (shouldBeLessThan)
+        if (this.value > shouldBeLessThan) return false;
+
+    if (shouldBeGreaterThan)
+        if (this.value < shouldBeGreaterThan) return false;
+
+    let leftValid = validBST(this.left, this.value, null);
+    let rightValid = validBST(this.right, null, this.value);
+
+    return (leftValid && rightValid)
 }
 
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+module.exports = { BinaryTree: BinaryTree, validBST: validBST };
