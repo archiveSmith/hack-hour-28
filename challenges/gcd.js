d4b16d87e15b73b850c00dc224e4c075420da1ce
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-05-03 09:31:41 -0700

    completed gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,18 @@
  */
 
 function gcd(a, b) {
-
+    let gcd = 1;
+    for (let i = 1; i <= Math.min(a, b); i += 1) {
+        if (a % i === 0 && b % i === 0) gcd = i;
+    }
+    return gcd;
 }
+
+// console.log(gcd(10, 8));
+// console.log(gcd(10, 9));
+// console.log(gcd(50, 5));
+// console.log(gcd(100, 80));
+// console.log(gcd(100, 99))
+// console.log(gcd(99, 6))
 
 module.exports = gcd;
