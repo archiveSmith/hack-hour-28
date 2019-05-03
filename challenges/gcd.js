c743b281bd64c68b70d8a8156975b38027b92fd4
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-05-03 10:16:24 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,36 @@
  *
  */
 
-function gcd(a, b) {
 
+function grabFactors(num) {
+  let i, j;
+  const results = []
+  num % 2 === 0 ? (i = 2, j = 1) : (i = 3, j = 2)
+  for(i; i <= (Math.floor(num / 2)); i += j){
+    if(num % i === 0)results.push(i)
+  }
+  if(results.length === 0) {
+    results.push(1);
+    results.push(num);
+  }
+  return results;
 }
+
+function gcd(a, b) {
+  let smallerNumArr;
+  let largerNumSet;
+  if(a < b){
+    smallerNumArr = grabFactors(a);
+    largerNumSet = new Set(grabFactors(b));
+  }else {
+    smallerNumArr = grabFactors(b);
+    largerNumSet = new Set(grabFactors(a));
+  }
+  for(i = smallerNumArr.length - 1; i >= 0; i -= 1){
+    if(largerNumSet.has(smallerNumArr[i])) return smallerNumArr[i];
+  }
+  return 1
+}
+
 
 module.exports = gcd;
