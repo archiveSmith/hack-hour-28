eb6edc69f988011b3e5202bf03bf473ab3394ea5
Keller <keller@Kellers-MacBook-Air.local>
2019-05-03 09:33:04 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,21 @@
  */
 
 function gcd(a, b) {
+  const max = Math.max(a, b)
+  const min = Math.min(a, b);
 
+  const common = [];
+
+  for (let i = 1; i <= min; i++) {
+    if (max % i === 0 && min % i === 0) {
+      common.push(i);
+    }
+  }
+
+  console.log(common);
+  return common[common.length - 1];
 }
+
+console.log(gcd(100, 10));
 
 module.exports = gcd;
