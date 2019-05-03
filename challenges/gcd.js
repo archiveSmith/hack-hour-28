4204e8eba110ebce2f2a4c2e0aa5a05d715a3c63
Sam Selfridge <Sam.selfridge@gmail.com>
2019-05-03 09:29:23 -0700

    for loops are better when they execute

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,23 @@
  */
 
 function gcd(a, b) {
+    let largest = (a < b) ? b : a;
+    let halfLargest = Math.ceil(largest / 2);
 
+    for (let i = halfLargest; i > 0; i--) {
+        // console.log(`i${i} -- ${a}  -- ${b}  `);
+        if(a % i === 0 && b % i === 0) return i;
+    }
+
+    return undefined;
 }
+
+
+// console.log(gcd(10,8));
+// console.log(gcd(10,8));
+// console.log(gcd(16,8));
+
+
+
 
 module.exports = gcd;
