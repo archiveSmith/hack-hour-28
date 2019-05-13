fc6f4894d5afb5f77c181f855a061c1677df0596
Raffi <fikash@gmail.com>
2019-05-13 09:26:14 -0700

    count twos solution attempt 1

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,9 +8,21 @@
 // countTwos(1000);  -> 300
 // countTwos(11420);  -> 4483
 
+// Create a counting variable
+// Create a loop that counts up from 1 to n
+// Convert number to string and split. 
+// Iterate over array of stringified digits and increment counting variable every time function encounters "2"
+// Return variable
 
 function countTwos(num) {
-
+  let counter = 0;
+  for (let i = 1; i <= num; i++) {
+    let stringy = i.toString().split("");
+    for (let j = 0; j < stringy.length; j++) {
+      if (stringy[j] === "2") counter++;
+    }
+  }
+  return counter;
 }
 
 module.exports = countTwos;
