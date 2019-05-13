0ea52afa1565ac3a2aab2f0bac5c4fb8863b4d5b
alyviam <alyvialmoss@gmail.com>
2019-05-13 09:36:44 -0700

    Alyvia's Hack-Hour Senior

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -10,7 +10,28 @@
 
 
 function countTwos(num) {
+	if (num < 2) {
+		return 0;
+	} else if (num === 2) {
+		return 1;
+	}
 
+	let twoCount = 0;
+	for (let i = 2; i <= num; i++) {
+		//console.log('this is i', i)
+		let numToStr = i.toString();
+		//console.log('numToStr', numToStr)
+		for (let j = 0; j < numToStr.length; j++) {
+			//console.log('numToStr[j]: ', numToStr[j])
+			if (numToStr[j] === '2') {
+				twoCount += 1;
+			}
+		}
+	}
+	return twoCount;
 }
-
+console.log(countTwos(1000));  //-> 300
+console.log(countTwos(13)); // -> 2
+console.log(countTwos(3));  //-> 1
+console.log(countTwos(11420));  //-> 4483
 module.exports = countTwos;
