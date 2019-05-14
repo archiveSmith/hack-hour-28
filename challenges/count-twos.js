024a87e0f08288ef7d30dcb988b565b3061b1d17
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-05-14 09:05:56 -0700

    change

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -5,11 +5,12 @@
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
 // countTwos(13);  -> 2
-// countTwos(1000);  -> 300
-// countTwos(11420);  -> 4483
+    // countTwos(1000);  -> 300
+    // countTwos(11420);  -> 4483
 
 
-function countTwos(num) {
+const countTwos = (num) => {
+
 
 }
 
