0847321928f451914997fe677c72e651481f74bc
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-05-15 09:47:37 -0700

    superbalance

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -1,7 +1,7 @@
 /* You are given an array of integers and a target number. Write a function that returns true if
  * there is a subset of the array that sums up to the target and returns false otherwise. A subset
  * can be any size and the elements do not have to appear consecutively in the array.
- * 
+ *
  * subsetSum([3, 7, 4, 2], 5) - > true, 3 + 2 = 5
  * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
  * subsetSum([8, 2, 4, 12], 13) -> false
@@ -9,7 +9,14 @@
  */
 
 function subsetSum(array, target) {
-
+  array.forEach((el)=>{
+    console.log("Current el",el);
+    array.forEach((el)=>{
+      console.log("Other el:",el);
+    });
+  });
 }
+
+subsetSum([8, -2, 1, -3], 6)
 
 module.exports = subsetSum;
--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -11,10 +11,27 @@ function BinaryTree(value) {
   this.value = value;
   this.left = null;
   this.right = null;
+  this.checkHeight = (left = 0, right = 0) => {
+    let potentialHeightLeft = left;
+    let potentialHeightRight = right;
+    if(this.left) potentialHeightLeft = this.left.checkHeight(left + 1, right);
+    if (this.right) potentialHeightRight = this.right.checkHeight(left, right + 1);
+    return Math.max(potentialHeightLeft, potentialHeightRight)
+  };
 }
 
 function superbalanced(tree) {
+  //check height of left of tree
+  const leftHeight = tree.left.checkHeight();
+  //check height of right of tree
+  const rightHeight = tree.right.checkHeight();
+  //max height of left/right can only be one more than opposite
+  if(leftHeight > rightHeight) return leftHeight - rightHeight <= 1;
+  else if(leftHeight < rightHeight) return rightHeight - leftHeight <= 1;
+  else return true;
+
 
 }
+
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
