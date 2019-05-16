b2dc39786a04d74749c832d3573f64765920d778
RadicalRoy <rolandquintana1991@gmail.com>
2019-05-16 09:51:22 -0700

    balanced

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,86 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  //empty tree
+  if(!tree || (tree.left == null && tree.right == null)) {
+    return true;
+  }
 
+  if(!tree.isBalanced()) return false;
+
+  if(Math.abs(height(tree.right) - height(tree.left)) > 1) return false;
+
+  return true;
 }
+
+BinaryTree.prototype.isBalanced = function () {
+  if(this.left) {
+    if(this.left.value > this.value) return false;
+    if(!this.right) return this.left.isBalanced();
+  }
+  if(this.right) {
+    if(this.right.value < this.value) return false;
+    if(!this.left) return this.right.isBalanced();
+  }
+  if(!this.right && !this.left) return true;
+
+  return this.right.isBalanced() && this.left.isBalanced();
+}
+
+function height (tree) {
+  if(!tree) return 0;
+  let q = [tree];
+  let h = 0;
+  let children = [];
+  while(q.length > 0) {
+    while(q.length > 0) {
+      let node = q.pop();
+      if(node.left) children.push(node.left);
+      if(node.right) children.push(node.right);
+      console.log(node.value);
+    }
+
+    q = children;
+    children = [];
+    h++; console.log(h);
+  }
+  return h;
+}
+
+
+
+BinaryTree.prototype.add = function (value) {
+  if(value < this.value) {
+    if(this.left) {
+      return this.left.add(value);
+    } else {
+      this.left = new BinaryTree(value);
+    }
+  } else if(value > this.value) {
+      if(this.right) {
+        return this.right.add(value);
+      } else {
+        this.right = new BinaryTree(value);
+      }
+  }
+}
+
+let a = new BinaryTree(5);
+a.add(3);
+a.add(6);
+a.add(7);
+a.add(2);
+a.add(4);
+a.add(1);
+a.add(-1);
+a.add(-2);
+a.add(-3);
+a.add(-4);
+//console.log(height(a));
+
+console.log(superbalanced(a));
+a.left.value = 10;
+console.log(superbalanced(a));
+
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
