88550e2ab247995637d16fe873a0a66a6355f1ad
Jonathan Peralez <jperalez@gmail.com>
2019-05-16 09:48:01 -0700

    I think this works

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,28 @@
  */
 
 function repeatNumbers(array) {
+  // O(n)
+  /* let numObj = {};
+  for (let i = 0; i < array.length; i++) {
+    if (numObj[array[i]]) {
+      return array[i];
+    } else {
+      numObj[array[i]] = true;
+    }
+  } */
+  const set = new Set(array);
+  const setSum = [...set].reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
 
+  const sum = array.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+  return sum - setSum;
 }
+
+const arr1 = [1,2,3,4,5,6,7,8,5,9];
+
+repeatNumbers(arr1); //?
 
 module.exports = repeatNumbers;
