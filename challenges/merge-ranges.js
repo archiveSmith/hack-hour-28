83f99c1c4da2d905fb0fd8f9b3a13b87575444ad
Jonathan Peralez <jperalez@gmail.com>
2019-05-18 09:16:09 -0700

    not finished

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,42 @@
  * Do not assume the ranges are in order
  */
 
-
+/* function flattenArray(array) {
+    let cache = []
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        array[i].forEach(num => {
+          cache.push(num);
+        });
+      }
+    }
+    return cache;
+  }
+  const sortedArr = flattenArray(array).sort((a,b) => (a-b));
+  
+  function mergeArray(array) {
+    const cache = [];
+    let counter = 0;
+    let innerArr = [];
+    for (let i = 0; i < array.length; i++) {
+      if (counter < 2) {
+        innerArr.push(array[i]);
+        counter++;
+      }
+      if (counter === 2) {
+        cache.push(innerArr);
+        counter = 0;
+        innerArr = [];
+      }
+    }
+    console.log(cache);
+    return cache; */
 function mergeRanges(array) {
-
+  
+  }
+  return mergeArray(sortedArr);
 }
+
+console.log(mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]));
 
 module.exports = mergeRanges;
