49d29a3dc244fc409525770bb885686f1759a2ff
Sam Selfridge <Sam.selfridge@gmail.com>
2019-05-18 09:31:51 -0700

    'Default Hack Hour Commit Message'

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -12,14 +12,6 @@
  *
  * Create a function that accepts a linked list and returns true if the linked list has a cyclical reference
  *
- * var node1 = new Node('1');
- * var node2 = node1.next = new Node('2');
- * var node3 = node2.next = new Node('3');
- * var node4 = node3.next = new Node('4');
- * var node5 = node4.next = new Node('5');
- * hasCycle(node1); // => false
- * node5.next = node2;
- * hasCycle(node1); // => true
  *
  * Challenge 1: Do this in linear time
  * Challenge 2: Do this in constant space
@@ -27,13 +19,44 @@
  *
  */
 
-var Node = function(value) {
+var Node = function (value) {
   this.value = value;
   this.next = null;
 }
 
-function hasCycle(head) {
+// a circular reference will result in an infanite loop
+// if code is still running after checktime, see if the current node appears again
+// checkTime should be much longer than expected to run through entire list
+function hasCycle(head, checkTime = 1000) {
 
+  const startTime = Date.now();
+  let checkNode;
+  while (head) {
+    if (Date.now() - startTime > checkTime) {
+      if (checkNode) {
+        if (head == checkNode) return true;
+      } else {
+        checkNode = head;
+      }
+    }
+    head = head.next;
+  }
+  return false;
 }
 
-module.exports = {Node: Node, hasCycle: hasCycle}
+var node1 = new Node('1');
+var node2 = node1.next = new Node('2');
+var node3 = node2.next = new Node('3');
+var node4 = node3.next = new Node('4');
+var node5 = node4.next = new Node('5');
+
+out = hasCycle(node1);
+out
+// => false
+node5.next = node2;
+out = hasCycle(node1);
+out
+// => true
+
+
+module.exports = { Node: Node, hasCycle: hasCycle }
