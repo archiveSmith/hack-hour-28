f8e5341e543d823cd9de2df33675e6271654a687
Sam Selfridge <Sam.selfridge@gmail.com>
2019-05-20 09:21:17 -0700

    typos are fun

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -8,7 +8,7 @@
 function sumMultiples3Or5Below1000() {
   let sum = 0;
   for (let i = 0; i <= 1000 ; i++) {
-    if(i % 3 === 0 || i % 5 === 0) sum +=sum;
+    if(i % 3 === 0 || i % 5 === 0) sum += i;
   }
   return sum;
 }
