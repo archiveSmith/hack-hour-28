173b83a99c661f3003aae36d870c62b3c3431646
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2019-05-20 09:31:28 -0700

    3o5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -5,9 +5,23 @@
 // write a function that will find the sum of all the multiples of 3 or 5
 // below 1000 and return that sum.
 
+//3-5-6-9 = 23
+//10-12-15-18 = 55
+//20-21-24-25-27 = 92
+//30-33-35-36-39 = 173
+//40-42-45-48 =
+
+
+
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  let num = 1
+  for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i
+      console.log(sum)
+    }
+  }
   return sum;
 }
 
@@ -15,10 +29,14 @@ function sumMultiples3Or5Below1000() {
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
-  let sum = 0;
-
-  return sum;
-}
+    let sum = 0;
+    for (let i = 0; i < z; i++) {
+      if (i % y === 0 || i % x === 0) {
+        sum += i
+      }
+    }
+    return sum;
+  }
 
 const objectToExport = {
   sumMultiples3Or5Below1000,
