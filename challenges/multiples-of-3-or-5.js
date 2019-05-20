cc8a46a089256077770eac7f1168073074e947fb
Sam Selfridge <Sam.selfridge@gmail.com>
2019-05-20 09:07:18 -0700

    It cannot be that easy..

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,9 +7,13 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  for (let i = 0; i <= 1000 ; i++) {
+    if(i % 3 === 0) sum += i; 
+    if(i % 5 === 0) sum += i; 
+  }
   return sum;
 }
+
 
 
 // extension make it dynamic function that takes input x,y,z
