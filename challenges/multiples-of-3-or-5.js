40a2a830b7c6e9c1d4a20c4b7f16809ff630b2bd
RadicalRoy <rolandquintana1991@gmail.com>
2019-05-20 09:12:25 -0700

    ez p z

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -8,8 +8,14 @@
 function sumMultiples3Or5Below1000() {
   let sum = 0;
 
+  for(let i = 3; i < 1000; i++) {
+    if(i % 3 === 0 || i % 5 === 0) sum += i;
+  }
+
   return sum;
 }
+
+//console.log(sumMultiples3Or5Below1000());
 
 
 // extension make it dynamic function that takes input x,y,z
@@ -17,8 +23,14 @@ function sumMultiples3Or5Below1000() {
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
 
+  for(let i = 1; i < z; i++) {
+    if(i % x === 0 || i % y === 0)  sum += i;
+  }
+
   return sum;
 }
+
+//console.log(sumMultiplesXOrYBelowZ(3, 5, 1000));
 
 const objectToExport = {
   sumMultiples3Or5Below1000,
