b7abee400d7880de9c76349a3bd6ea38599ca602
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-05-20 09:38:55 -0700

    HackHour 5-20-19

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,7 +7,9 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  for (let i = 3; i < 1000; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
+  };
   return sum;
 }
 
@@ -16,7 +18,9 @@ function sumMultiples3Or5Below1000() {
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
-
+  for (let i = 1; i < z; i += 1) {
+    if (i % x === 0 || i % y === 0) sum += i;
+  };
   return sum;
 }
 
@@ -24,5 +28,10 @@ const objectToExport = {
   sumMultiples3Or5Below1000,
   sumMultiplesXOrYBelowZ,
 };
+
+
+console.log(`${sumMultiples3Or5Below1000()} -> Should equal: 233168`);
+console.log(`${sumMultiplesXOrYBelowZ(3, 5, 10)} -> Should equal: 23`);
+
 
 module.exports = objectToExport;
