bef556ebd9b9ba085cfdf206d434d7f34271665c
julicious100 <beakjulie@gmail.com>
2019-05-21 09:34:39 -0700

    hackhour may/21

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,8 +7,19 @@
  *
  */
 
-function maxSubarray(arr) {
+// Kadanes'algorithm
 
+function maxSubarray(arr) {
+  let currentSum = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    let currentN = arr[i];
+    currentSum = Math.max(currentSum + currentN, 0);
+    maxSum = Math.max(currentSum, maxSum);
+  }
+  return maxSum;
 }
+console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
 
 module.exports = maxSubarray;
