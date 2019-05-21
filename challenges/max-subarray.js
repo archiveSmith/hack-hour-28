1f73a9b4ff76f00194b8a17ac5de140a56b2fdf2
Keith <keith.avila@gmail.com>
2019-05-21 09:24:09 -0700

    hack hour

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,17 @@
  */
 
 function maxSubarray(arr) {
-
+  if (!Array.isArray(arr) || arr.length === 0) return "not a valid array";
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      if (sum > max) max = sum;
+    }
+  }
+  return max;
 }
-
+maxSubarray([1]); //?
+maxSubarray(15); //?
 module.exports = maxSubarray;
