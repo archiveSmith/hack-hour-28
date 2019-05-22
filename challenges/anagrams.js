f2cda155f7b4cd4f0dcb357d3c7124fde9156bbb
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-05-22 09:37:12 -0700

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,23 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
 
+//time complexity 
+function anagrams(string) {
+  const result = new Set([string]);
+  strArr = string.split('');
+  //repeat action for each character in the string
+  for(let count = 0; count < string.length; count += 1){
+    for(let i = 0; i < string.length - 1; i += 1){
+      let temp = strArr[i + 1];
+      strArr[i + 1] = strArr[i];
+      strArr[i] = temp;
+      result.add(strArr.join(''));
+    }
+  }
+  return Array.from(result);
 }
+const result = anagrams('abc');
+console.log(result);
 
 module.exports = anagrams;
