6e71844ee8b9ce4409b6fddaff2b9cb321193633
Sam Ryoo <samryoo@gmail.com>
2019-05-22 09:45:09 -0700

    unique values in array

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,26 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
 
+ function anagrams(string) {
+	let result = [];
+	string = string.split("");
+	function inner(stringy, newStr) {
+    console.log(`this is newStr ${newStr}`)
+    console.log(`this is stringy.length ${stringy.length}`)
+		if(!stringy.length) {
+			result.push(newStr.join(""));
+		}
+    console.log(`this is result ${result}`)
+		for(let i = 0; i < stringy.length; i++) {
+			let copy = stringy.slice();
+			copy.splice(i,1);
+			inner(copy, newStr.concat(stringy[i]));
+		}
+	}
+	inner(string, []);
+	return result;
 }
+anagrams('dog');
 
 module.exports = anagrams;
