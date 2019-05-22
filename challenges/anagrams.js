76e75c2728aff4c5e7e989ef319db32ce24c1c8b
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-05-22 09:42:22 -0700

    hh5-22-19

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -1,7 +1,7 @@
 /**
- * Given a single input string, write a function that produces all possible anagrams
- * of a string and outputs them as an array. At first, don't worry about
- * repeated strings.  What time complexity is your solution?
+ Given a single input string, write a function that produces all possible anagrams
+ of a string and outputs them as an array. At first, don't worry about
+ repeated strings.  What time complexity is your solution?
  *
  * Extra credit: Deduplicate your return array without using uniq().
  */
@@ -13,7 +13,24 @@
   */
 
 function anagrams(string) {
-
+  const anagrams = new Set();
+  string = string.split('');
+  const doIt = (string, anagram) => {
+    if (!string.length) {
+      anagrams.add(anagram.join(''));
+    }
+    for (let i = 0; i < string.length; i++) {
+      let copy = string.slice();
+      copy.splice(i, 1);
+      doIt(copy, anagram.concat(string[i]));
+    }
+  }
+  doIt(string, []);
+  return Array.from(anagrams);
 }
+
+const testStr = anagrams('abc');
+console.log(testStr); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+
 
 module.exports = anagrams;
