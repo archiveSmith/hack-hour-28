4650fa5ff9cea87ede46c12d584e2c52d6a6b7c6
RadicalRoy <rolandquintana1991@gmail.com>
2019-05-23 09:45:21 -0700

    gotm

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -14,6 +14,23 @@
 
 function anagrams(string) {
 
+  let grams = [string];
+  for(let i = 0; i < string.length; i++) {
+    grams = grams.reduce((acc, ele) => {
+      for(let j = i; j < ele.length; j++) {
+        let strArr = ele.split('');
+        [strArr[i], strArr[j]] = [strArr[j], strArr[i]];
+        strArr = strArr.join('');
+        acc.push(strArr); //ele with ith and jth entries swapped
+      }
+      return acc;
+    }, [])
+  }
+
+  return grams;
+
 }
+
+//console.log(anagrams('abc'));
 
 module.exports = anagrams;
