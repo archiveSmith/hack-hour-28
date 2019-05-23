00a3f14ba72cdd6559dd3c71a9ad784f9432425e
Aidan Noble-Goodman <anoblegoodman@Aidans-MacBook-Pro.local>
2019-05-23 09:48:10 -0700

    checking again

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,23 +10,29 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
+function LinkedList(){
+  this.head = null;
+}
+
+
+//make 2 pointers;
+//the first pointer should point at the head while the second pointer moves down the rest of the linked list comparing its value to the first pointer's value
+//if a duplicate is found, delete it
+//if no duplicates are found by the time the second pointer is pointing to null, increment the first counter to be the second node in the linked list
+//set the second pointer to be the third node in the linked list and loop until you hit null again, deleting any duplicates;
+//repeat until both pointers are pointing at the same node;
 
 function deleteDups(head) {
-  let current = head;
-  let nextPointer = current.next;
-  if(head === null) {return undefined};
-  while(nextPointer != null){
-    if(current != nextPointer){
-      current = nextPointer;
-      nextPointer = nextPointer.next;
+  let p1 = head;
+  let p2 = p1.next;
+  while(p2){
+    if(p1 === p2){
+      p2 = p2.next;
+      delete p2;
     }
-    if(current === nextPointer){
-      current = nextPointer
-      nextPointer = nextPointer.next;
-      delete current;
-    } 
+    p1 = p1.next;
+    p2 = p1.next.next;
   }
-  return head;
 }
 
 
