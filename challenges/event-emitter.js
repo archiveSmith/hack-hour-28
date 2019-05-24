6ffdf662225bb792048edf6366e434710682402a
Jonathan Peralez <jperalez@gmail.com>
2019-05-24 09:45:33 -0700

    event-emitter works

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -14,23 +14,35 @@
  *
  * Caveats:
  * - If we repeatedly call .on with the same event name, it should
- *   continue to call the old function(s) as well. That is to say, we can have multiple
+ *   continue to call the old function( s) as well. That is to say, we can have multiple
  *   listeners for one event.
  * - If `obj.trigger` is called with additional arguments, pass those to the
  *   listeners.
  * - It is not necessary to write a way to remove listeners.
  */
 
-function EventEmitter() {
+let counter = 0;
 
+function EventEmitter() {
+  this.funcDictionary = {};
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  this.funcDictionary[funcName] = func;
+  // return this.funcDictionary;
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  this.funcDictionary[funcName](...args);
 };
+
+/* const myEmitter = new EventEmitter();
+
+myEmitter.on('increment', function(){
+  counter++;
+});
+
+myEmitter.trigger('increment');
+counter; */
 
 module.exports = EventEmitter;
