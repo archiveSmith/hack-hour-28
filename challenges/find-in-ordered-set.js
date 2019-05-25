c6e8d81560e432fe24677a65677ae5a81fd9dce9
Aidan Noble-Goodman <anoblegoodman@Aidans-MacBook-Pro.local>
2019-05-25 09:22:06 -0700

    checking find-in-ordered-set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,28 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  let start = 0;
+  let end = arr.length - 1;
+  while(start <= end){
+    //find the element that's closest to the middle of the array 
+    let middle = Math.floor((start + end)/ 2)
+    //if the middle is the target, return true;
+    if(arr[middle] === target) return true;
+    //if middle is not the target, determine if the target is to the right or the left of the middle;
+    else if(arr[middle] < target){
+      //if the middle value is smaller than the target value, search only to the right of the middle;
+      start = middle + 1;
+    } else {
+      //if the middle value is larger than the target value, search only to the left of the middle;
+      end = middle - 1;
+    }
+  }
+  //if the element is never found, return false;
+  return false;
 }
+
+const nums = [1, 2, 3, 4, 5, 6, 7]
+console.log(findInOrderedSet(nums, 8))
 
 
 module.exports = findInOrderedSet;
