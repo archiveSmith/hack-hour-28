d31d20b8b369d5529c8ca9a20144e9d413875488
Keith <keith.avila@gmail.com>
2019-05-25 09:50:56 -0700

    hh

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -7,12 +7,22 @@ var nums = [1, 4, 6, 7, 9, 17, 45]
 findInOrderedSet(nums, 4);  -> true
 findInOrderedSet(nums, 2);  -> false
 
- */
-
+*/
 
 function findInOrderedSet(arr, target) {
-
+  let high = arr.length - 1;
+  let low = 0;
+  while (high - low > 1) {
+    let mid = low + Math.floor((high - low) / 2);
+    if (arr[mid] === target || arr[low] === target || arr[high] === target)
+      return true;
+    else if (arr[mid] > target) high = mid;
+    else low = mid;
+  }
+  return false;
 }
 
+var nums = [1, 4, 6, 7, 9, 17, 45];
+findInOrderedSet(nums, 46); //?
 
 module.exports = findInOrderedSet;
