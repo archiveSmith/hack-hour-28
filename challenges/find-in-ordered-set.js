b40d23c338203fa6009b3056f089cca4fd0cdc49
Keller <keller@Kellers-MacBook-Air.local>
2019-05-25 09:49:43 -0700

    find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,30 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
-
 function findInOrderedSet(arr, target) {
+  let max = arr.length - 1;
+  let min = 0;
+  let guess;
+  let steps = 1;
 
+  while (min <= max) {
+    guess = Math.floor((max + min) / 2);
+    if (arr[guess] === target) {
+      return true;
+    } else if (arr[guess] < target) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
+    }
+    steps++;
+    console.log(steps);
+  }
+
+  return false;
 }
 
+var nums = [1, 4, 6, 7, 9, 17, 45];
+
+console.log(findInOrderedSet(nums, 4));
 
 module.exports = findInOrderedSet;
