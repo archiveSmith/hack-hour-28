d25e39167ede7c690e711d62b2491b0dd0976a69
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-05-25 09:29:14 -0700

    hh 5-25-19

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,72 @@
 
 
 function deleteDups(head) {
-
+  if (head === undefined) return "Undefined";
+  if (head.next === null) return head;
+  const dupsObj = {};
+  let curr = head;
+  const holder ={
+    start: null,
+    end: null
+  }
+  while (curr) {
+    const strNum = curr.value.toString();
+    dupsObj.strNum = curr.value
+    let nextValue;
+    if (curr.next !== null) {
+      nextValue = curr.next.value.toString();
+    } else {
+      const nextValue = 'undefined'
+    }
+    if (curr.next.value === dupsObj.nextValue) {
+      if (holder.start === null) {
+        holder.start = curr
+      } else {
+        holder.next = cur
+      }
+    }
+    if (curr.next.value !== dupsObj.nextValue) {
+      if (holder.start !== null && holder.end !== null) {
+        holder.start.next = curr.next;
+        holder.start = null;
+        holder.end = null
+      }
+      if (holder.start !== null && holder.end === null) {
+        holder.start.next = curr.next;
+        holder.start = null;
+      }
+    }
+    curr = curr.next;
+  }
+  return head;
 }
+
+
+
+const node4 = {
+  value: 4,
+  next: null
+}
+
+const node3 = {
+  value: 3,
+  next:node4
+}
+const node2 = {
+  value: 2,
+  next: node3
+}
+const node1 = {
+  value: 2,
+  next: node2
+}
+
+const head= {
+  value: 1,
+  next: node1
+}
+
+console.log(deleteDups(head))
+
 
 module.exports = deleteDups;
--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,21 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  const middle = Math.ceil(arr.length / 2)
+  if (arr[middle] === target) return true
+  if (arr.length === 1) return false;
+  if (target < arr[middle]) {
+    const tempArr = arr.slice(0, middle)
+    return findInOrderedSet(tempArr, target)
+  } else {
+    const tempArr = arr.slice(middle + 1, arr.slice(2, arr.length))
+    return findInOrderedSet(tempArr, target)
+  }
 }
+
+const nums = [1, 4, 6, 7, 9, 17, 45]
+console.log(`${findInOrderedSet(nums, 4)} -> Shoule equal: true`)
+console.log(`${findInOrderedSet(nums, 2)} -> Should equal: false`)
 
 
 module.exports = findInOrderedSet;
