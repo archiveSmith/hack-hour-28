8f4566affe5b3a415056e441e8452a69077e4576
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-05-28 09:43:08 -0700

    in srt

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+    for(let i = array.length - 1; i >= 0; i -= 1){
+      const currVal = array[i];
+      let nextVal = i - 1;
+      while(currVal < array[nextVal] && nextVal >= 0){
+        array[nextVal + 1] = array[nextVal];
+        array[nextVal] = currVal;
+        nextVal--;
+      }
+    }
 }
+
 
 module.exports = insertionSort;
