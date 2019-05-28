e1051be761bb01d017b658bec31bfa2fc9f72268
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-05-28 09:29:57 -0700

    completed insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,24 @@
 
 
 function insertionSort(array) {
-
+    for (let i = 1; i < array.length; i += 1) {
+        let key = array[i];
+        // console.log(key)
+        let j = i - 1;
+        // console.log(j)
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            // console.log(array[j+1])
+            // console.log(array)
+            j = j - 1;
+            // console.log(j)
+        }
+        array[j + 1] = key;
+    }
+    return array;
 }
+
+// let arr = [5, 2, -6, 17, 101, 0, 3] // ---> [-6, 0, 2, 3, 5, 17, 101]
+// console.log(insertionSort(arr))
 
 module.exports = insertionSort;
