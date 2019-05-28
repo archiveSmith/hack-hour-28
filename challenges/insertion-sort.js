abf3976916bb599ebf5f78d68a9d58cf228746e2
Sam Selfridge <Sam.selfridge@gmail.com>
2019-05-28 09:18:58 -0700

    flashbacks to js fundamentals

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,28 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
+    console.log(`Array to Sort:`);
+    console.log(array);
+  array.forEach((element, index) => {
 
+    for(i = 0;i < index ; i++){
+        // console.log(`Inserting ${array[index]} into ${array}`);
+        if(array[index] < array[i]){
+            // console.log(`Swapping i arr[${i}]:${array[i]} with index arr[${index}]:${array[index]}`);
+            [array[index],array[i]] = [array[i],array[index]]
+        }
+    }
+  });
 }
+
+
+// const array = [1,3,6,14,2,7,8,7]
+
+// console.log(array);
+// insertionSort(array);
+// console.log();
+// console.log(array);
+
 
 module.exports = insertionSort;
