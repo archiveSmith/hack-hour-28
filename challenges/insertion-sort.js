417cfb42a93fe1ffa2f218ba1c4cf2088f93b0c8
Keith <keith.avila@gmail.com>
2019-05-28 09:35:15 -0700

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,24 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
+function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
 
 function insertionSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j - 1] > array[j]) {
+      swap(array, j, --j);
+    }
+    arr;
+  }
 }
+
+let arr = [4, 2, 1];
+insertionSort(arr);
+arr;
 
 module.exports = insertionSort;
