06bbb45df9fc7cd0e8d562ccf19f3545a5a5ce7e
Keller <keller@Kellers-MacBook-Air.local>
2019-05-28 09:48:29 -0700

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,27 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
+  const len = array.length;
+  //create new array to store sorted values
 
+  //loop over array
+  for (let i = 0; i < len; i++) {
+    let curr = array[i];
+    let j;
+
+    //loop over newArr
+    for (j = i - 1; j >= 0 && array[j] > curr; j--) {
+      //for given element, check if less than next value in new array
+      //if yes, add to current index
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = curr;
+  }
+  //return sorted values
+  return array;
 }
+
+console.log(insertionSort([4, 2, 6, 3]));
 
 module.exports = insertionSort;
