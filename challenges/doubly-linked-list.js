bb14d56fe98b686e0544e86252e3697ae78bc9e9
Keller <keller@Kellers-MacBook-Air.local>
2019-05-30 09:42:01 -0700

    doublylinkedlist v0.1

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,39 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  const node = new Node(val);
+  if (head === null) {
+    head = node;
+  } else {
+    let currentNode = head;
+
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = node;
+    node.prev = currentNode;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let currentNode = head;
+  let previousNode;
+
+  if (currentNode.val === val) {
+    head = currentNode.next;
+    delete currentNode;
+    head.prev = null;
+  } else {
+    while (currentNode.val !== val) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
+    previousNode.next = currentNode.next;
+  }
 };
 
 module.exports = LinkedList;
