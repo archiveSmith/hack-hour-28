898d266734d8e8575a35e2a49349b13bf8e9a743
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-06-01 09:24:55 -0700

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -4,8 +4,16 @@
 
 
 
-function bubbleSort(array) {
-
+const bubbleSort = (array)  => {
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < array.length; j++){
+      if(array[j] > array[j+1]){
+        array[j] = array[j+1];
+        array[j+1] = temp
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
