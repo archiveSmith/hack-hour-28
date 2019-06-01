73cb4bce0ac411eca8dcddec9e6243edcfd74518
j218 <jaymlim93@gmail.com>
2019-06-01 09:45:03 -0700

    finished bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,21 @@
 
 
 function bubbleSort(array) {
-
+    // iterate through with 2 pointers 
+    let flag = true;
+    while(flag) {
+        flag = false
+        for(let i = 0; i < array.length; i++) {
+            if(array[i] > array[i+1]) {
+                [array[i], array[i+1]] = [array[i+1], array[i]]
+                flag = true;
+            }
+        }
+    }
+    return array;
 }
+// let array = [5, 9, 13, 4, 1, 6];
+// console.log(bubbleSort(array))
+
 
 module.exports = bubbleSort;
