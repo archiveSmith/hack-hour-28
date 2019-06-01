6c6cbf1f351f3e71486dc416ab6f5a68f7c0aac0
Keller <keller@Kellers-MacBook-Air.local>
2019-06-01 09:33:00 -0700

    bubsort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,21 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
 function bubbleSort(array) {
+  let len = array.length;
 
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
 }
+
+console.log(bubbleSort([2, 7, 1, 6, 4, 5]));
 
 module.exports = bubbleSort;
