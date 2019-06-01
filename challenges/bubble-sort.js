3113b067380511589f8d88565a5eefda29796875
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-06-01 09:39:36 -0700

    completed bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,21 @@
 
 
 function bubbleSort(array) {
-
+    let sortedArr = array;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < sortedArr.length; i += 1) {
+            if (sortedArr[i] < sortedArr[i - 1]) {
+                [sortedArr[i], sortedArr[i - 1]] = [sortedArr[i - 1], sortedArr[i]];
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return sortedArr;
 }
+
+// let arr = [5, -10, 100, 65, 591, 2, 0];
+// console.log(bubbleSort(arr))
 
 module.exports = bubbleSort;
