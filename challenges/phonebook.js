013b508513cb6b1a4b50084672d6499c67a800bd
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-06-03 09:38:31 -0700

    hh 6-3-19

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -18,21 +18,36 @@
 *
 * How efficient can you make this?
 
-* Part 2: 
+* Part 2:
 * Why are we storing names and phone numbers in an array?
-* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook 
+* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook
 * complete with methods to add new names and look up and remove existing entries
 */
 
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  for (let i = 0; i < jazbook.length; i += 1) {
+    if (jazbook[i][0] === name) {
+      return jazbook[i][1]
+    }
+  }
+  return false;
 }
 
 // return an object literal representing the jazbook
 function makePhoneBookObject(jazbook){
-
+  const obj = {};
+  for (let i = 0; i < jazbook.length; i += 1) {
+    obj[jazbook[i][0]] = jazbook[i][1]
+  }
+  return obj;
 }
+
+const book = [['alex', '301-844-3421'], ['jae', '301-844-1211'], ['david', '301-844-0978'], ['travis', '301-844-8505'], ['jasmine', '1800-974-4539']];
+
+console.log(`${findName(book, 'jae')} --> Should be: 301-844-1211`);
+console.log(`${findName(book, 'Mike')} --> Should be: false`);
+console.log(`${makePhoneBookObject(book)} --> Should be: {alex: '301-844-3421', jae: '301-844-1211', david: '301-844-0978', travis: '301-844-8505', jasmine: '1800-974-4539'}`);
 
 const objectToExport = {
   findName,
