5d4009059ee3fead20a93df8c659d372621ac095
Sam Selfridge <Sam.selfridge@gmail.com>
2019-06-05 10:31:53 -0700

    tricky lines

--- challenges/new-intersections.js
+++ challenges/new-intersections.js
@@ -17,31 +17,8 @@
  *
  */
 
-function newIntersections(x, y) {
-  const minX = Math.min(...x);
-  const minY = Math.min(...y);
-
-  const maxX = Math.max(...x);
-  const maxY = Math.max(...y);
-
-  //check all points in the range
-
-  let count = 0;
-
-  for (let i = minX; i <= maxX; i++) {
-    for (let j = minY; j <= maxY; j++) {
-      
-    }
-  }
-}
-
-function newIntersectionsFirstRun(x, y) {
-  const subX = [];
-  const subY = [];
-  const subObj = {};
-  let count = 0;
-  // find matching x points
-
+function getLines(x, y) {
+  const subObjX = {};
   for (let i = 0; i < x.length; i++) {
     let foundFlag = false;
     for (let j = i + 1; j < x.length; j++) {
@@ -49,39 +26,68 @@ function newIntersectionsFirstRun(x, y) {
       const checkX = x[j];
 
       if (currentX === checkX) {
-        if (!subObj[currentX]) subObj[currentX] = new Set();
-        subObj[currentX].add(y[i]);
-        subObj[currentX].add(y[j]);
+        if (!subObjX[currentX]) subObjX[currentX] = new Set();
+        subObjX[currentX].add(y[i]);
+        subObjX[currentX].add(y[j]);
       }
     }
   }
-  console.log(`SubObj`);
-  console.log(subObj);
 
-  for (const y in subObj) {
-    if (subObj.hasOwnProperty(y)) {
-      const element = subObj[y];
+  for (const y in subObjX) {
+    if (subObjX.hasOwnProperty(y)) {
+      const element = subObjX[y];
       const newArray = [];
       newArray[0] = Math.min(...element);
       newArray[1] = Math.max(...element);
-      subObj[y] = newArray;
+      subObjX[y] = newArray;
     }
   }
+  return subObjX;
+}
 
-  for (const yKey in subObj) {
-    if (subObj.hasOwnProperty(yKey)) {
-      const rangeArr = subObj[yKey];
+function newIntersections(x, y) {
+  let count = 0;
+  // find matching x points
 
-      for (let i = 0; i < y.length; i++) {
-        for (let j = i; j < y.length; j++) {
-          const currentY = y[i];
-          const checkY = y[j];
+  const subObjX = getLines(x, y);
+  const subObjY = getLines(y, x);
 
-          if (currentY === checkY) count++;
+  // console.log(subObjX);
+  // console.log(subObjY);
+
+  xLines = Object.keys(subObjX).length;
+  yLines = Object.keys(subObjY).length;
+
+  let lines1, lines2; //lines one will be the longer group
+
+  if (xLines > yLines) {
+    lines1 = subObjX;
+    lines2 = subObjY;
+  } else {
+    lines1 = subObjY;
+    lines2 = subObjX;
+  }
+
+  for (const val1 in lines1) {
+    if (lines1.hasOwnProperty(val1)) {
+      const range1 = lines1[val1];
+      for (const val2 in lines2) {
+        if (lines2.hasOwnProperty(val2)) {
+          const range2 = lines2[val2];
+          let int1 = parseInt(val1);
+          let int2 = parseInt(val2);
+
+          // console.log(`${range1[0]} <= ${val2} && ${val2} <= ${range1[1]}`)
+          // console.log(`${range2[0]} <= ${val1} && ${val1} <= ${range2[1]}`)
+          // console.log();
+
+          if (  range1[0] <= int2 && int2 <= range1[1])
+            if (range2[0] <= int1 && int1 <= range2[1]) count++;
         }
       }
     }
   }
+
   return count;
 }
 let x, y;
@@ -96,31 +102,31 @@ y = [2, 2, 1, 0];
 
 x = [-1, -3, -2, -2];
 y = [-2, -2, -1, -3];
-// console.log(newIntersections(x,y));
+console.log(newIntersections(x,y));
 
 x = [0.1, 0.3, 0.2, 0.2];
 y = [0.2, 0.2, 0.1, 0.3];
 
-// console.log(newIntersections(x,y));
+console.log(newIntersections(x,y));
 
 x = [1, 3, 2, 2, 0];
 y = [2, 2, 1, 3, 3];
 
-// console.log(newIntersections(x,y));
+console.log(newIntersections(x,y));
 
 x = [-1, -2, 10, 2, 3, 3, 5, 5, 6];
 y = [8, -5, -1, 3, 4, 9, 1, 6, 3];
 
-// console.log(newIntersections(x,y));
+console.log(newIntersections(x,y));
 
 x = [-1, -2, 10, 2, 3, 3, 5, 5, 6, 9];
 y = [8, -5, -1, 3, 4, 9, 1, 6, 3, 3];
 
-// console.log(newIntersections(x,y));
+console.log(newIntersections(x,y));
 
-x = [-1, -2, 10, 2, 3, 3, 5, 5, 6, 9, 9, 12];
-y = [5, -5, -1, 3, 4, 9, 1, 6, 3, 3, 10, 5];
+x = [-1, -2, 10, 2, 3, 3, 5, 5, 6, 9, 9 , 12];
+y = [5 , -5, -1, 3, 4, 9, 1, 6, 3, 3, 10, 5];
 
-// console.log(newIntersections(x,y));
+console.log(newIntersections(x,y));
 
 module.exports = newIntersections;
