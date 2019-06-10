1d753fa12772cab8ead3e2c2b34a71f43971378c
Sam Selfridge <Sam.selfridge@gmail.com>
2019-06-10 09:11:04 -0700

    pick one or the other

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,7 +15,14 @@
  */
 
 function countStairs(n) {
+  if (n === 0) return 1;
+  if (n < 0) return 0;
 
+  return countStairs(n - 2) + countStairs(n - 1);
 }
+
+// let count = countStairs(5)
+// console.log(`Cont of ${5}: ${count}`);
+
 
 module.exports = countStairs;
