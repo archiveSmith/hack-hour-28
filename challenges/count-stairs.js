d004ab51595e5806dbd8817cc400e3d19d58f339
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-06-10 09:26:32 -0700

    count stairs pt 2

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,8 +14,25 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
-function countStairs(n) {
+function countStairs(n, str='') {
+    if(n === 1) {
+        console.log(str + 1);
+        return str + 1;
+    }
+    if(n === 2){
+        countStairs(1, str + '1 + ');
+        console.log(str + 2);
+        return str + 2;
+    }
+    if(n > 1) {
+        const newStr = str + '1 + '
+        countStairs(n - 1, newStr);
+    }
+    if(n > 2){
+        const newStr = str + '2 + ';
+        countStairs(n - 2, newStr);
+    }
 
 }
-
+countStairs(10);
 module.exports = countStairs;
