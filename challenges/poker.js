e447ac81624d05d8b07a177a91a986067855e4c5
Sam Selfridge <Sam.selfridge@gmail.com>
2019-06-11 09:29:00 -0700

    early birds get the untested worm

--- challenges/poker.js
+++ challenges/poker.js
@@ -15,12 +15,92 @@
  *
  * Clearly real-world poker has more complex rules for tied ranks, but we want you
  * to focus on overall architecture rather than edge cases. Have fun!
- * 
+ *
  * BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
-function poker(hand1, hand2) {
 
+// helper functions for different hands.  If true return the value of the card, -1 if false
+// EXPECTS SORTED ARRAY
+function is4ofKind(hand) {
+  if (hand[0] === hand[3] || hand[1] === hand[5]) return hand[1];
+  return -1;
 }
+
+function isFullHouse(hand) {
+  if (hand[0] === hand[2] && hand[3] === hand[4]) return hand[2];
+  if (hand[0] === hand[1] && hand[2] === hand[4]) return hand[2];
+  return -1;
+}
+
+function isStraight(hand) {
+  let straightFlag = true;
+  for (let i = 1; i < hand.length; i++) {
+    if (hand[i - 1] + 1 !== hand[i]) straightFlag = false;
+  }
+
+  return straightFlag ? hand[4] : -1;
+}
+
+function is3ofKind(hand) {
+  for (let i = 2; i < hand.length; i++) {
+    if (hand[i - 2] === hand[i - 1] && hand[i - 1] === hand[i]) return hand[i];
+  }
+
+  return -1;
+}
+
+function is2Pair(hand) {
+  if (
+    (hand[0] === hand[1] && hand[2] === hand[3]) || //last card out
+    (hand[1] === hand[2] && hand[3] === hand[4]) || // first card
+    (hand[0] === hand[1] && hand[3] === hand[4])  // middle card
+  )
+   
+    return hand[3];
+
+  return -1;
+}
+
+function is1Pair(hand){
+    let pairFlag = false;
+    for (let i = 1; i < hand.length; i++) {
+        if(hand[i-1] === hand[i]) return hand[i];        
+    }
+    return -1
+}
+
+
+const handArray = [
+    is4ofKind, isFullHouse,isStraight,is3ofKind,is2Pair,is1Pair
+]
+
+function poker(hand1, hand2) {
+    hand1.sort();
+    hand2.sort();
+    for (let i = 0; i < handArray.length; i++) {
+        const func = handArray[i];
+
+        const hand1Value = func(hand1)
+        const hand2Value = func(hand2)
+
+        if( hand1Value < hand2Value ) return "Player 2 wins"
+        if( hand1Value > hand2Value ) return "Player 1 wins"
+        
+    }
+    // No one has anything, highest unique card wins
+    for (let i = 4; i < hand1.length; i--) {
+        if(hand1[i] < hand2[i]) return "Player 2 wins"
+        if(hand1[i] > hand2[i]) return "Player 1 wins"        
+    }
+    return "Draw!!"
+}
+
+const onePair = [4,6,8,8,9]
+const twoPair = [2, 2, 5, 7, 7];
+const threeOKind = [5, 8, 8, 8, 9];
+const fourOKind = [6, 8, 8, 8, 8];
+const straight = [5, 6, 7, 8, 9];
+const fullHouse = [2, 2, 2, 4, 4];
 
 module.exports = poker;
