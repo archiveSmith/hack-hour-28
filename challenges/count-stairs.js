834c413fb9d74028de33fbf055b34b6587e02257
Keller <keller@The-Person-Next-To-You.local>
2019-06-11 10:03:59 -0700

    lots of hack hours

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,7 +15,21 @@
  */
 
 function countStairs(n) {
+  // if (n === 0) return 0;
+  // if (n === 1) return 1;
+  // if (n === 2) return 2;
 
+  // var arr = [1, 2];
+  // for (let i = 2; i < n; i++) {
+  //   arr[i] = arr[i - 1] + arr[i - 2];
+  // }
+  // return arr[n - 1];
+
+  let arr = [1, 2, 3];
+  for (let i = 3; i < 2; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n - 1];
 }
 
 module.exports = countStairs;
--- challenges/poker.js
+++ challenges/poker.js
@@ -15,12 +15,101 @@
  *
  * Clearly real-world poker has more complex rules for tied ranks, but we want you
  * to focus on overall architecture rather than edge cases. Have fun!
- * 
+ *
  * BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
 function poker(hand1, hand2) {
+  const handValues = {
+    "four of a kind": 19,
+    "full house": 18,
+    straight: 17,
+    "three of a kind": 16,
+    "2-pair": 15,
+    "1-pair": 14,
+    ace: 13,
+    king: 12,
+    queen: 11,
+    jack: 10,
+    "10": 9,
+    "9": 8,
+    "8": 7,
+    "7": 6,
+    "6": 5,
+    "5": 4,
+    "4": 3,
+    "3": 2,
+    "2": 1
+  };
 
+  //puts cards in order
+  const sortedHand1 = hand1.sort((a, b) => a - b);
+  const sortedHand2 = hand2.sort((a, b) => a - b);
+
+  //runs over hands and counts number of each card
+  const hand1CardCount = cardCounter(sortedHand1);
+  const hand2CardCount = cardCounter(sortedHand2);
+
+  //checks card count obj and determines hand type
+  let hand1Type = handChecker(hand1CardCount);
+  let hand2Type = handChecker(hand2CardCount);
+
+  console.log(handValues[hand1Type]);
+  console.log(hand2CardCount);
+
+  console.log(hand1Type);
+
+  return handValues[hand1Type] > handValues[hand2Type]
+    ? "hand1 wins"
+    : "hand2 wins";
+}
+
+console.log(poker([3, 5, 5, 5, 2], [6, 1, 5, 8, 10]));
+
+function cardCounter(hand) {
+  const handCardCount = {};
+  for (let i = 0; i < hand.length; i++) {
+    if (!handCardCount[hand[i]]) {
+      handCardCount[hand[i]] = 0;
+    }
+    handCardCount[hand[i]]++;
+  }
+  return handCardCount;
+}
+
+function handChecker(handObj) {
+  let handType;
+
+  for (const prop in handObj) {
+    //check hands in descending order
+    if (handObj[prop] === 4) {
+      handType = "four of a kind";
+    }
+
+    if (handObj[prop] === 3) {
+      if (handType === "1-pair") {
+        handType = "full house";
+      } else {
+        handType = "three of a kind";
+      }
+    }
+
+    if (handObj[prop] === 2) {
+      if (handType === "1-pair") {
+        handType = "2-pair";
+      } else if (handType === "three of a kind") {
+        handType = "full house";
+      } else {
+        handType = "1-pair";
+      }
+    }
+
+    if (handObj[prop] === 1) {
+      handType = prop;
+    }
+  }
+
+  return handType;
 }
 
 module.exports = poker;
