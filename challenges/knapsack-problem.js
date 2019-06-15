c4fc7f834ea10e9e8da609c07b59fb5ffada3c30
RadicalRoy <rolandquintana1991@gmail.com>
2019-06-15 09:45:44 -0700

    REEEEEEEEEduce

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -11,6 +11,37 @@
 
 function solveKnapsack(items, weightAvailable) {
 
+  let backpacks = [items];
+  let posValues = [0];
+
+  while(backpacks.length > 0)
+  for(let i = backpacks.length - 1; i >= 0; i--) { // iterating in reverse to remove backpacks
+    //console.log(backpacks[i].reduce((a, b) => a + b.weight, 0))
+    if(backpacks[i].reduce((a, b) => a + b.weight, 0) <= weightAvailable) {
+      posValues.push(backpacks[i].reduce((a, b) => a + b.value, 0));
+      // want to remove backpack from possible backpacks
+      backpacks.splice(i, 1);
+      continue;
+    }
+    let newBackpacks = []
+    console.log(backpacks);
+    // else remove 1 of each item from backpack and create new possible backpacks.
+    for(let j = 0; j < backpacks[i].length; j++) {
+      let pack = backpacks[i].slice();
+      pack.splice(j, 1);
+      if(pack.length > 0) newBackpacks.push(pack);
+    }
+    console.log(newBackpacks);
+    backpacks.splice(i, 1);
+    backpacks = backpacks.concat(newBackpacks);
+    console.log(backpacks)
+  }
+  //console.log(posValues)
+  return Math.max(...posValues);
+
 };
+//const items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+
+//console.log(solveKnapsack(items, 9));
 
 module.exports = solveKnapsack;
